299176b0e278e36a29e8b3565b8fa45758e850b4
Aldik <andi.kitta77@gmail.com>
2022-07-18 20:27:04 +0800

    DateJsonAdapter 2

--- app/src/main/java/com/example/movplayv3/data/api/DateJsonAdapter.kt
+++ app/src/main/java/com/example/movplayv3/data/api/DateJsonAdapter.kt
@@ -5,16 +5,19 @@ import com.squareup.moshi.FromJson
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonReader
 import com.squareup.moshi.JsonWriter
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
 class DateJsonAdapter : JsonAdapter<Date>() {
-    private companion object{
+    private companion object {
         @SuppressLint("SimpleDateFormat")
         val serverDateFormat = SimpleDateFormat("yyyy-MM-dd")
+
         @SuppressLint("SimpleDateFormat")
         val isoDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
     }
+
     @FromJson
     override fun fromJson(reader: JsonReader): Date? {
         val dateString = reader.nextString()
@@ -23,10 +26,22 @@ class DateJsonAdapter : JsonAdapter<Date>() {
             return null
         }
 
+        val serverDateFormat = serverDateFormat.parseOrNull(dateString)
+
         return null
     }
 
     override fun toJson(writer: JsonWriter, value: Date?) {
         TODO("Not yet implemented")
+    }
+}
+
+private fun SimpleDateFormat.parseOrNull(source: String): Date? {
+    return try {
+        parse(source)
+    } catch (e: ParseException) {
+        null
+    } catch (e: ArrayIndexOutOfBoundsException) {
+        null
     }
 }
