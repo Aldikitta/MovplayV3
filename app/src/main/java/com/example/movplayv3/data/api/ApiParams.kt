f136f204e03413027c141196f928fd11411ab376
Aldik <andi.kitta77@gmail.com>
2022-07-18 20:08:24 +0800

    ApiParams

---
+++ app/src/main/java/com/example/movplayv3/data/api/ApiParams.kt
@@ -0,0 +1,16 @@
+package com.example.movplayv3.data.api
+
+import kotlin.time.Duration.Companion.seconds
+
+object ApiParams {
+    const val secureBaseUrl = "https://api.themoviedb.org/3/"
+
+    //10 MB cache
+    const val cacheSize = (10 * 1024 * 1024).toLong()
+
+    object Timeouts {
+        val connect = 10.seconds
+        val write = 10.seconds
+        val read = 10.seconds
+    }
+}
