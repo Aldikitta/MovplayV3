894851666e9acc6dd1b7507dcc5c21adc9463271
Aldik <andi.kitta77@gmail.com>
2022-07-19 13:20:57 +0800

    AggregateCredits

--- app/src/main/java/com/example/movplayv3/data/model/AggregateCredits.kt
+++ app/src/main/java/com/example/movplayv3/data/model/AggregateCredits.kt
@@ -1,5 +1,6 @@
 package com.example.movplayv3.data.model
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
@@ -7,4 +8,23 @@ data class AggregateCredits(
     val cast: List<Cast>,
     val crew: List<Crew>,
     val id: Int
+)
+
+@JsonClass(generateAdapter = true)
+data class Cast(
+    val adult: Boolean,
+    val gender: Int?,
+    override val id: Int,
+    @Json(name = "known_for_department")
+    val knownForDepartment: String?,
+    val name: String,
+    val order: Int,
+    @Json(name = "original_name")
+    val originalName: String,
+    val popularity: Double,
+    @Json(name = "profile_path")
+    override val profilePath: String?,
+    val roles: List<Role>,
+    @Json(name = "total_episode_count")
+    val totalEpisodeCount: Int
 )
