8164957edf4761ab4b9d297ef44727872e8aaff9
Aldik <andi.kitta77@gmail.com>
2022-07-20 18:50:34 +0800

    MovieStatus

--- app/src/main/java/com/example/movplayv3/data/model/movie/MovieStatus.kt
+++ app/src/main/java/com/example/movplayv3/data/model/movie/MovieStatus.kt
@@ -1,9 +1,29 @@
 package com.example.movplayv3.data.model.movie
 
+import androidx.annotation.StringRes
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 enum class MovieStatus(val value: String) {
+    @Json(name = "Rumored")
+    Rumored("Rumored"),
+
+    @Json(name = "Planned")
+    Planned("Planned"),
+
+    @Json(name = "In Production")
+    InProduction("In Production"),
+
+    @Json(name = "Post Production")
+    PostProduction("Post Production"),
+
+    @Json(name = "Released")
+    Released("Released"),
+
+    @Json(name = "Cancelled")
+    Cancelled("Cancelled");
+    @StringRes
 
 
 }
