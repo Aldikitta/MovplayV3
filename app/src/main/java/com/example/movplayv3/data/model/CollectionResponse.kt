2d40110731593b7c3e42238aace1b431e5684f66
Aldik <andi.kitta77@gmail.com>
2022-07-20 12:49:14 +0800

    CollectionResponse

--- app/src/main/java/com/example/movplayv3/data/model/CollectionResponse.kt
+++ app/src/main/java/com/example/movplayv3/data/model/CollectionResponse.kt
@@ -1,5 +1,6 @@
 package com.example.movplayv3.data.model
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
@@ -7,5 +8,13 @@ data class CollectionResponse(
     val id: Int,
     val name: String,
     val overview: String?,
-    val parts: List<Part>
+    val parts: List<Part>,
+    @Json(name = "backdrop_path")
+    val backdropPath: String?,
+    @Json(name = "poster_path")
+    val posterPath: String?,
+    @Transient
+    val posterUrl: String? = null,
+    @Transient
+    val backdropUrl: String? = null
 )
