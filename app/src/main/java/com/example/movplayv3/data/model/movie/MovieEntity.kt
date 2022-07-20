59ccf5d4ec2fc78a7cf0bc2acecee8f592033078
Aldik <andi.kitta77@gmail.com>
2022-07-20 18:59:32 +0800

    MovieDetailsRemoteKey

---
+++ app/src/main/java/com/example/movplayv3/data/model/movie/MovieEntity.kt
@@ -0,0 +1,10 @@
+package com.example.movplayv3.data.model.movie
+
+import androidx.room.Entity
+import androidx.room.Index
+
+@Entity(indices = [Index(value = ["type", "language"])])
+data class MovieEntity(
+    override val id: Int,
+    val type: MovieEntityType,
+)
