56aaf2d406b17de5ac744072e6fa4b9cc3a342a0
Aldik <andi.kitta77@gmail.com>
2022-07-20 19:41:29 +0800

    SearchResponse

--- app/src/main/java/com/example/movplayv3/data/model/SearchResponse.kt
+++ app/src/main/java/com/example/movplayv3/data/model/SearchResponse.kt
@@ -1,9 +1,15 @@
 package com.example.movplayv3.data.model
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class SearchResponse(
     val page: Int,
-
+    @Json(name = "results")
+    val results: List<SearchResult>,
+    @Json(name = "total_pages")
+    val totalPages: Int,
+    @Json(name = "total_results")
+    val totalResults: Int
 )
