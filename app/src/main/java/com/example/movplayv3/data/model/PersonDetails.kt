736edaeaf9945a374d5d49caa6b01c7949da6d34
Aldik <andi.kitta77@gmail.com>
2022-07-20 19:25:00 +0800

    PersonDetails

---
+++ app/src/main/java/com/example/movplayv3/data/model/PersonDetails.kt
@@ -0,0 +1,29 @@
+package com.example.movplayv3.data.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import java.util.*
+
+@JsonClass(generateAdapter = true)
+data class PersonDetails(
+    val id: Int,
+    val birthday: Date?,
+    @Json(name = "deathday")
+    val deathDate: Date?,
+    @Json(name = "known_for_department")
+    val knownFor: String,
+    @Json(name = "also_knows_as")
+    val alsoKnowsAs: List<String>?,
+    val gender: Int,
+    val name: String,
+    val biography: String,
+    val popularity: Float,
+    @Json(name = "place_of_birth")
+    val birthPlace: String?,
+    @Json(name = "profile_path")
+    val profilePath: String?,
+    val adult: Boolean,
+    val homepage: String?,
+    @Json(name = "imdb_id")
+    val imdbId: String?
+)
