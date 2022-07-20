3ab8f12943fd6c3cd6ae5a6592bbc5b24eed768f
Aldik <andi.kitta77@gmail.com>
2022-07-20 12:56:53 +0800

    MediaType

--- app/src/main/java/com/example/movplayv3/data/model/CombinedCredits.kt
+++ app/src/main/java/com/example/movplayv3/data/model/CombinedCredits.kt
@@ -1,9 +1,23 @@
 package com.example.movplayv3.data.model
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import okhttp3.MediaType
 
 @JsonClass(generateAdapter = true)
 data class CombinedCredits(
     val cast: List<CombinedCreditsCast>,
     val crew: List<CombinedCreditsCrew>
+)
+
+@JsonClass(generateAdapter = true)
+data class CombinedCreditsCast(
+    override val id: Int,
+    val name: String?,
+    val character: String?,
+    override val title: String?,
+    @Json(name = "media_type")
+    override val mediaType: ,
+    @Json(name = "poster_path")
+    override val posterPath: String?
 )
