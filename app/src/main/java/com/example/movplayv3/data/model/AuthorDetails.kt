030ac9358e0a76eba2bb065975d088eff4f425e2
Aldik <andi.kitta77@gmail.com>
2022-07-20 12:28:20 +0800

    AuthorDetails

---
+++ app/src/main/java/com/example/movplayv3/data/model/AuthorDetails.kt
@@ -0,0 +1,15 @@
+package com.example.movplayv3.data.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class AuthorDetails(
+    val name: String,
+    val username: String,
+    @Json(name = "profile_path")
+    val avatarPath: String?,
+    val rating: Float?,
+    @Transient
+    val avatarUrl: String?
+)
