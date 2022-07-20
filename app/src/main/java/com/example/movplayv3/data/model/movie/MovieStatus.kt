8f502b1851dc61b939ebe782a7065da5184818b4
Aldik <andi.kitta77@gmail.com>
2022-07-20 18:49:18 +0800

    MovieStatus

--- app/src/main/java/com/example/movplayv3/data/model/movie/MovieStatus.kt
+++ app/src/main/java/com/example/movplayv3/data/model/movie/MovieStatus.kt
@@ -3,5 +3,7 @@ package com.example.movplayv3.data.model.movie
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-enum class MovieStatus {
+enum class MovieStatus(val value: String) {
+
+
 }
