b48684569c91c3b034d935646c8760356816fc8a
Aldik <andi.kitta77@gmail.com>
2022-07-20 13:30:23 +0800

    DateUtils

--- app/src/main/java/com/example/movplayv3/utils/DateUtils.kt
+++ app/src/main/java/com/example/movplayv3/utils/DateUtils.kt
@@ -7,3 +7,7 @@ fun Date.formatted(format: String = "dd.MM.yyyy"): String {
     val dateFormatter = SimpleDateFormat(format, Locale.getDefault())
     return dateFormatter.format(this)
 }
+
+fun Date.yearString() = formatted(format = "yyyy")
+
+
