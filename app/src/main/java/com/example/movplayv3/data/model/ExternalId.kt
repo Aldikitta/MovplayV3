df482d962e2e75ba7dc52da246779b6b0339bdc8
Aldik <andi.kitta77@gmail.com>
2022-07-20 14:33:50 +0800

    ExternalId

---
+++ app/src/main/java/com/example/movplayv3/data/model/ExternalId.kt
@@ -0,0 +1,22 @@
+package com.example.movplayv3.data.model
+
+import androidx.annotation.DrawableRes
+import com.example.movplayv3.R
+
+sealed class ExternalId(@DrawableRes val drawableRes: Int) {
+    data class Imdb(val id: String, val type: ExternalContentType) :
+        ExternalId(drawableRes = R.drawable.ic_imdb)
+
+    data class Facebook(val id: String, val type: ExternalContentType) :
+        ExternalId(drawableRes = R.drawable.ic_facebook)
+
+    data class Instagram(val id: String, val type: ExternalContentType) :
+        ExternalId(drawableRes = R.drawable.ic_instagram)
+
+    data class Twitter(val id: String, val type: ExternalContentType) :
+        ExternalId(drawableRes = R.drawable.ic_twitter)
+}
+
+enum class ExternalContentType {
+    Movie, Tv, Person;
+}
