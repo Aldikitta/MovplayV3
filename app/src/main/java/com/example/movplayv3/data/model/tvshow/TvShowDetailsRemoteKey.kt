a9921ccc736def169b1f235c47fce07f6efa8f7c
Aldik <andi.kitta77@gmail.com>
2022-07-21 21:42:08 +0800

    TvShowDetailsRemoteKey

--- app/src/main/java/com/example/movplayv3/data/model/tvshow/TvShowDetailsRemoteKey.kt
+++ app/src/main/java/com/example/movplayv3/data/model/tvshow/TvShowDetailsRemoteKey.kt
@@ -1,3 +1,13 @@
 package com.example.movplayv3.data.model.tvshow
 
-data class TvShowDetailsRemoteKey()
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(indices = [Index(value = ["language"])])
+data class TvShowDetailsRemoteKey(
+    @PrimaryKey(autoGenerate = false)
+    val language: String,
+    val nextPage: Int?,
+    val lastUpdated: Long
+)
