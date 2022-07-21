9dffb8f53a38b463c0317295a2db5e48a2dfd185
Aldik <andi.kitta77@gmail.com>
2022-07-21 21:47:01 +0800

    TvShowResponse

---
+++ app/src/main/java/com/example/movplayv3/data/model/tvshow/TvShowResponse.kt
@@ -0,0 +1,15 @@
+package com.example.movplayv3.data.model.tvshow
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class TvShowResponse(
+    val page: Int,
+    @Json(name = "results")
+    val tvSeries: List<TvShow>,
+    @Json(name = "total_pages")
+    val totalPages: Int,
+    @Json(name = "total_results")
+    val totalResults: Int
+)
