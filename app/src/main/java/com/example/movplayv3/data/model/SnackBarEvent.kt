4a2dda9f3cd20a6db6526ee10ad1125549e1ddfd
Aldik <andi.kitta77@gmail.com>
2022-07-21 21:00:22 +0800

    SnackBarEvent

--- app/src/main/java/com/example/movplayv3/data/model/SnackBarEvent.kt
+++ app/src/main/java/com/example/movplayv3/data/model/SnackBarEvent.kt
@@ -1,3 +1,9 @@
 package com.example.movplayv3.data.model
 
-sealed class SnackBarEvent()
+import androidx.annotation.StringRes
+import com.example.movplayv3.R
+
+sealed class SnackBarEvent(@StringRes val messageStringRes: Int) {
+    object NetworkDisconnected : SnackBarEvent(R.string.snack_bar_network_disconnected_label)
+    object NetworkConnected : SnackBarEvent(R.string.snack_bar_network_connected_label)
+}
