2223615604fc2868004091c05c5c71a6dedcb479
Aldik <andi.kitta77@gmail.com>
2022-07-21 20:58:00 +0800

    ShareDetails

--- app/src/main/java/com/example/movplayv3/data/model/ShareDetails.kt
+++ app/src/main/java/com/example/movplayv3/data/model/ShareDetails.kt
@@ -2,7 +2,15 @@ package com.example.movplayv3.data.model
 
 data class ShareDetails(
     val title: String,
-    val imbdId: ExternalId.Imdb
-){
+    val imdbId: ExternalId.Imdb
+) {
+    fun asMessage(): String {
+        val contentLink = when (imdbId.type) {
+            ExternalContentType.Movie, ExternalContentType.Tv -> "title"
+            else -> "name"
+        }
+        val url = "https://www.imdb.com/$contentLink/${imdbId.id}"
 
+        return "$title\n\n$url"
+    }
 }
