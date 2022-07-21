0a4c6e00aff4c24a394f86e6101594d2788ac527
Aldik <andi.kitta77@gmail.com>
2022-07-21 21:54:11 +0800

    MovieStatus

--- app/src/main/java/com/example/movplayv3/data/model/movie/MovieStatus.kt
+++ app/src/main/java/com/example/movplayv3/data/model/movie/MovieStatus.kt
@@ -5,7 +5,7 @@ import com.example.movplayv3.R
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
+@JsonClass(generateAdapter = false)
 enum class MovieStatus(val value: String) {
     @Json(name = "Rumored")
     Rumored("Rumored"),
@@ -26,11 +26,10 @@ enum class MovieStatus(val value: String) {
     Cancelled("Cancelled");
 
     @StringRes
-
     fun getLabel(): Int = when (this) {
         Rumored -> R.string.status_rumored_label
         Planned -> R.string.status_planned_label
-        InProduction -> R.string.status_planned_label
+        InProduction -> R.string.status_in_production_label
         PostProduction -> R.string.status_post_production_label
         Released -> R.string.status_released_label
         Cancelled -> R.string.status_cancelled_label
