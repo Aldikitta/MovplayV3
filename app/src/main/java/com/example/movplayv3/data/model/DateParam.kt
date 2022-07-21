52aaf809e0ef68b4c1e7756f606ec8aaddcd3b5f
Aldik <andi.kitta77@gmail.com>
2022-07-21 21:51:03 +0800

    DateParam

--- app/src/main/java/com/example/movplayv3/data/model/DateParam.kt
+++ app/src/main/java/com/example/movplayv3/data/model/DateParam.kt
@@ -1,13 +1,9 @@
 package com.example.movplayv3.data.model
 
 import com.example.movplayv3.utils.formatted
-import com.squareup.moshi.JsonClass
 import java.util.*
 
-@JsonClass(generateAdapter = true)
-data class DateParam(
-    private val date: Date
-){
+data class DateParam(private val date: Date) {
     override fun toString(): String {
         return date.formatted("yyyy-MM-dd")
     }
