97c950b4968e4be6805d403ac4f9734e3f754feb
Aldik <andi.kitta77@gmail.com>
2022-07-21 21:37:25 +0800

    TvShowStatus

--- app/src/main/java/com/example/movplayv3/data/model/tvshow/TvShowStatus.kt
+++ app/src/main/java/com/example/movplayv3/data/model/tvshow/TvShowStatus.kt
@@ -1,5 +1,36 @@
 package com.example.movplayv3.data.model.tvshow
 
-enum class TvShowStatus {
-    TopRated, AiringToday, Favourite, RecentlyBrowsed, Trending, OnTheAir
+import androidx.annotation.StringRes
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = false)
+enum class TvShowStatus(val value: String) {
+    @Json(name = "Returning Series")
+    Returning("Returning Series"),
+
+    @Json(name = "Planned")
+    Planned("Planned"),
+
+    @Json(name = "In Production")
+    InProduction("In Production"),
+
+    @Json(name = "Ended")
+    Ended("Ended"),
+
+    @Json(name = "Canceled")
+    Canceled("Canceled"),
+
+    @Json(name = "Pilot")
+    Pilot("Pilot");
+
+    @StringRes
+    fun getLabel(): Int = when (this) {
+        Returning -> R.string.tv_series_status_returning_label
+        Planned -> R.string.tv_series_status_planned_label
+        InProduction -> R.string.tv_series_status_in_production_label
+        Ended -> R.string.tv_series_status_ended_label
+        Canceled -> R.string.tv_series_status_cancelled_label
+        Pilot -> R.string.tv_series_status_pilot_label
+    }
 }
---
+++ app/src/main/java/com/example/movplayv3/data/model/tvshow/TvShowType.kt
@@ -0,0 +1,5 @@
+package com.example.movplayv3.data.model.tvshow
+
+enum class TvShowType {
+    TopRated, AiringToday, Favourite, RecentlyBrowsed, Trending, OnTheAir
+}
