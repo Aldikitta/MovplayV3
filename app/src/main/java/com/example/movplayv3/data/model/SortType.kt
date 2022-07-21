6ccbaf2a9ecfc575107157060daadbe4726bd01b
Aldik <andi.kitta77@gmail.com>
2022-07-21 21:05:03 +0800

    SortType

--- app/src/main/java/com/example/movplayv3/data/model/SortType.kt
+++ app/src/main/java/com/example/movplayv3/data/model/SortType.kt
@@ -1,4 +1,21 @@
 package com.example.movplayv3.data.model
 
+import androidx.annotation.StringRes
+import com.example.movplayv3.R
+
 enum class SortType {
+    Popularity, VoteCount, ReleaseDate, VoteAverage, OriginalTitle;
+
+    fun toSortTypeParam(order: SortOrder) = when (this) {
+
+    }
+
+    @StringRes
+    fun getLabelResId(): Int = when (this) {
+        Popularity -> R.string.sort_type_popularity_label
+        ReleaseDate -> R.string.sort_type_release_date_label
+        VoteAverage -> R.string.sort_type_vote_average_label
+        OriginalTitle -> R.string.sort_type_title_label
+        VoteCount -> R.string.sort_type_vote_count_label
+    }
 }
