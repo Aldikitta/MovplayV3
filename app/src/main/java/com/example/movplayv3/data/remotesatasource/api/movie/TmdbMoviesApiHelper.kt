aa2e8d57b2799e8587b5c1f2f73d6c52187b0400
Aldik <andi.kitta77@gmail.com>
2022-07-22 19:53:02 +0800

    TmdbMoviesApiHelper

--- app/src/main/java/com/example/movplayv3/data/remotesatasource/api/movie/TmdbMoviesApiHelper.kt
+++ app/src/main/java/com/example/movplayv3/data/remotesatasource/api/movie/TmdbMoviesApiHelper.kt
@@ -30,4 +30,10 @@ interface TmdbMoviesApiHelper {
         isoCode: String = DeviceLanguage.default.languageCode,
         region: String = DeviceLanguage.default.region,
     ): MoviesResponse
+
+    suspend fun getTopRatedMovies(
+        page: Int,
+        isoCode: String = DeviceLanguage.default.languageCode,
+        region: String = DeviceLanguage.default.region,
+    ): MoviesResponse
 }
