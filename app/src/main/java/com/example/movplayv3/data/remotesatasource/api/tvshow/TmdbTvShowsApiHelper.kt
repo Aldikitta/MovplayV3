a166b6857f41aa7502b3cc1ebc171b0c55f2ede7
Aldik <andi.kitta77@gmail.com>
2022-07-22 19:41:00 +0800

    TmdbTvShowsApiHelper

---
+++ app/src/main/java/com/example/movplayv3/data/remotesatasource/api/tvshow/TmdbTvShowsApiHelper.kt
@@ -0,0 +1,21 @@
+package com.example.movplayv3.data.remotesatasource.api.tvshow
+
+import com.example.movplayv3.data.model.*
+import com.example.movplayv3.data.model.tvshow.TvShowsResponse
+import retrofit2.Call
+
+interface TmdbTvShowsApiHelper {
+    fun getConfig(): Call<Config>
+
+    suspend fun discoverTvShows(
+        page: Int,
+        isoCode: String = DeviceLanguage.default.languageCode,
+        region: String = DeviceLanguage.default.region,
+        sortTypeParam: SortTypeParam = SortTypeParam.PopularityDesc,
+        genresParam: GenresParam = GenresParam(genres = emptyList()),
+        watchProvidersParam: WatchProvidersParam = WatchProvidersParam(watchProviders = emptyList()),
+        voteRange: ClosedFloatingPointRange<Float> = 0f..10f,
+        fromAirDate: DateParam? = null,
+        toAirDate: DateParam? = null
+    ): TvShowsResponse
+}
