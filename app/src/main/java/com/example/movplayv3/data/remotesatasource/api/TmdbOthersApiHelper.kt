e190cab377b9e97e9e5c54598bd20cb6be10c7d5
Aldik <andi.kitta77@gmail.com>
2022-07-22 20:01:30 +0800

    TmdbOthersApiHelper

---
+++ app/src/main/java/com/example/movplayv3/data/remotesatasource/api/TmdbOthersApiHelper.kt
@@ -0,0 +1,20 @@
+package com.example.movplayv3.data.remotesatasource.api
+
+import com.example.movplayv3.data.model.Config
+import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.model.SearchResponse
+import retrofit2.Call
+
+interface TmdbOthersApiHelper {
+    fun getConfig(): Call<Config>
+
+    suspend fun multiSearch(
+        page: Int,
+        isoCode: String = DeviceLanguage.default.languageCode,
+        region: String = DeviceLanguage.default.region,
+        query: String,
+        includeAdult: Boolean = false,
+        year: Int? = null,
+        releaseYear: Int? = null
+    ): SearchResponse
+}
