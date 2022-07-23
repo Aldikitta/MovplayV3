d49557e73c0e0856b03641c47f5329d37340b5f7
Aldik <andi.kitta77@gmail.com>
2022-07-23 20:59:14 +0800

    MoviesDetailsDao

--- app/src/main/java/com/example/movplayv3/data/local/db/movie/MoviesDetailsDao.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/movie/MoviesDetailsDao.kt
@@ -1,10 +1,15 @@
 package com.example.movplayv3.data.local.db.movie
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Query
 import androidx.room.TypeConverters
+import com.example.movplayv3.data.model.movie.MovieDetailEntity
 import com.example.movplayv3.utils.MovieEntityTypeConverters
 
 @TypeConverters(MovieEntityTypeConverters::class)
 @Dao
 interface MoviesDetailsDao {
+    @Query("SELECT * FROM MovieDetailEntity WHERE language=:language")
+    fun getAllMovies(language: String): PagingSource<Int, MovieDetailEntity>
 }
