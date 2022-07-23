3ac695e28e8ca224bdee3b97b0cf774c74198384
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:02:38 +0800

    MoviesRemoteKeysDao

---
+++ app/src/main/java/com/example/movplayv3/data/local/db/movie/MoviesRemoteKeysDao.kt
@@ -0,0 +1,10 @@
+package com.example.movplayv3.data.local.db.movie
+
+import androidx.room.Dao
+import androidx.room.TypeConverters
+import com.example.movplayv3.utils.MovieEntityTypeConverters
+
+@TypeConverters(MovieEntityTypeConverters::class)
+@Dao
+interface MoviesRemoteKeysDao {
+}
