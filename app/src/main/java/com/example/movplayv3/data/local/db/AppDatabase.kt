8cfde55965cb3119e38531eca67186502217fef1
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:30:02 +0800

    AppDatabase

--- app/src/main/java/com/example/movplayv3/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/AppDatabase.kt
@@ -3,9 +3,8 @@ package com.example.movplayv3.data.local.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.movplayv3.data.local.db.movie.FavoritesMoviesDao
-import com.example.movplayv3.data.local.db.movie.MoviesDao
-import com.example.movplayv3.data.local.db.tvshow.FavoritesTvShowsDao
+import com.example.movplayv3.data.local.db.movie.*
+import com.example.movplayv3.data.local.db.tvshow.*
 import com.example.movplayv3.data.model.SearchQuery
 import com.example.movplayv3.data.model.movie.*
 import com.example.movplayv3.data.model.tvshow.*
@@ -31,8 +30,21 @@ import com.example.movplayv3.utils.DateConverters
 )
 @TypeConverters(DateConverters::class)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun favoritesMoviesDao(): FavoritesMoviesDao
+    //Movies
     abstract fun moviesDao(): MoviesDao
-    abstract fun favoritesTvShowsDao(): FavoritesTvShowsDao
+    abstract fun favoritesMoviesDao(): FavoritesMoviesDao
+    abstract fun recentlyBrowsedMovies(): RecentlyBrowsedMoviesDao
+    abstract fun moviesRemoteKeysDao(): MoviesRemoteKeysDao
+    abstract fun moviesDetailsDao(): MoviesDetailsDao
+    abstract fun moviesDetailsRemoteKeys(): MoviesDetailsRemoteKeysDao
 
+    //Tv Shows
+    abstract fun tvShowsDao(): TvShowsDao
+    abstract fun favoritesTvShowDao(): FavoritesTvShowsDao
+    abstract fun recentlyBrowsedTvShows(): RecentlyBrowsedTvShowsDao
+    abstract fun tvShowsRemoteKeysDao(): TvShowsRemoteKeysDao
+    abstract fun tvShowsDetailsDao(): TvShowsDetailsDao
+    abstract fun tvShowsDetailsRemoteKeys(): TvShowsDetailsRemoteKeysDao
+
+    abstract fun searchQueryDao(): SearchQueryDao
 }
