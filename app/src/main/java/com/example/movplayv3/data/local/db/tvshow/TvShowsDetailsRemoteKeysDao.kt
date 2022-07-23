27efbd1488805950c40f30db3850d1d3a9ad72bf
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:21:50 +0800

    TvShowsDetailsRemoteKeysDao

--- app/src/main/java/com/example/movplayv3/data/local/db/tvshow/TvShowsDetailsRemoteKeysDao.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/tvshow/TvShowsDetailsRemoteKeysDao.kt
@@ -1,10 +1,18 @@
 package com.example.movplayv3.data.local.db.tvshow
 
-import androidx.room.Dao
-import androidx.room.TypeConverters
+import androidx.room.*
+import com.example.movplayv3.data.model.tvshow.TvShowDetailsRemoteKey
 import com.example.movplayv3.utils.TvShowEntityTypeConverters
 
 @TypeConverters(TvShowEntityTypeConverters::class)
 @Dao
 interface TvShowsDetailsRemoteKeysDao {
+    @Query("SELECT * FROM TvShowDetailsRemoteKey WHERE language=:language LIMIT 1")
+    suspend fun getRemoteKey(language: String): TvShowDetailsRemoteKey?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertKey(remoteKey: TvShowDetailsRemoteKey)
+
+    @Query("DELETE FROM TvShowDetailsRemoteKey WHERE language=:language")
+    suspend fun deleteKeys(language: String)
 }
