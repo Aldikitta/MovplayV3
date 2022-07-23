cc2c18313a022c5662a50b868e352c2ff75712e0
Aldik <andi.kitta77@gmail.com>
2022-07-23 12:24:19 +0800

    FavoritesMoviesDao

--- app/src/main/java/com/example/movplayv3/data/localdatasource/db/AppDatabase.kt
+++ app/src/main/java/com/example/movplayv3/data/localdatasource/db/AppDatabase.kt
@@ -3,9 +3,9 @@ package com.example.movplayv3.data.localdatasource.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.movplayv3.data.model.SearchQuery
-import com.example.movplayv3.data.model.movie.*
-import com.example.movplayv3.data.model.tvshow.*
+import com.example.movplayv3.model.SearchQuery
+import com.example.movplayv3.model.movie.*
+import com.example.movplayv3.model.tvshow.*
 import com.example.movplayv3.utils.DateConverters
 
 @Database(
@@ -16,18 +16,18 @@ import com.example.movplayv3.utils.DateConverters
         RecentlyBrowsedTvShow::class,
         SearchQuery::class,
         MovieEntity::class,
-        MoviesRemoteKeys::class,
         TvShowEntity::class,
+        MoviesRemoteKeys::class,
         TvShowsRemoteKeys::class,
         MovieDetailEntity::class,
-        MovieDetailsRemoteKey::class,
         TvShowDetailEntity::class,
+        MovieDetailsRemoteKey::class,
         TvShowDetailsRemoteKey::class
     ],
     version = 1
 )
 @TypeConverters(DateConverters::class)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun
+//    abstract fun
 
 }
