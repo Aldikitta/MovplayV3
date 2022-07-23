6da9e2893db6b9178775e82fd952e687bf985647
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:15:33 +0800

    SearchQueryDao

--- app/src/main/java/com/example/movplayv3/data/local/db/SearchQueryDao.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/SearchQueryDao.kt
@@ -1,7 +1,16 @@
 package com.example.movplayv3.data.local.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.movplayv3.data.model.SearchQuery
 
 @Dao
 interface SearchQueryDao {
+    @Query("SELECT `query` FROM SearchQuery WHERE `query` LIKE '%' || :query || '%' ORDER BY last_use_date DESC LIMIT 3")
+    suspend fun searchQueries(query: String): List<String>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addQuery(searchQuery: SearchQuery)
 }
