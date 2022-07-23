c3c8991370a42708a0bdd440072217e621cab85e
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:48:59 +0800

    ConfigDataSource

--- app/src/main/java/com/example/movplayv3/data/paging/ConfigDataSource.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/ConfigDataSource.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -38,5 +39,7 @@ class ConfigDataSource @Inject constructor(
             Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH),
             0
         )
-    }
+
+        emit(activities.isNotEmpty())
+    }.flowOn(defaultDispatcher)
 }
