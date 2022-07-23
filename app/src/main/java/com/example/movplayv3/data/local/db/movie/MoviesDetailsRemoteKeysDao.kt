40333fecfd315ccb841133622e5133daec643134
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:01:40 +0800

    MoviesDetailsRemoteKeysDao

--- app/src/main/java/com/example/movplayv3/data/local/db/movie/MoviesDetailsRemoteKeysDao.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/movie/MoviesDetailsRemoteKeysDao.kt
@@ -1,10 +1,18 @@
 package com.example.movplayv3.data.local.db.movie
 
-import androidx.room.Dao
-import androidx.room.TypeConverters
+import androidx.room.*
+import com.example.movplayv3.data.model.movie.MovieDetailsRemoteKey
 import com.example.movplayv3.utils.MovieEntityTypeConverters
 
 @TypeConverters(MovieEntityTypeConverters::class)
 @Dao
 interface MoviesDetailsRemoteKeysDao {
+    @Query("SELECT * FROM MovieDetailsRemoteKey WHERE language=:language LIMIT 1")
+    suspend fun getRemoteKey(language: String): MovieDetailsRemoteKey?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertKey(remoteKey: MovieDetailsRemoteKey)
+
+    @Query("DELETE FROM MovieDetailsRemoteKey WHERE language=:language")
+    suspend fun deleteKeys(language: String)
 }
