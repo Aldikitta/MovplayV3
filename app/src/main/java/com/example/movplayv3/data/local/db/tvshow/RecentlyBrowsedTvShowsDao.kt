44724e4e332ba8bcd7d3edf07c86687029f0cfb3
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:12:26 +0800

    RecentlyBrowsedTvShowsDao

--- app/src/main/java/com/example/movplayv3/data/local/db/tvshow/RecentlyBrowsedTvShowsDao.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/tvshow/RecentlyBrowsedTvShowsDao.kt
@@ -10,14 +10,30 @@ import com.example.movplayv3.data.model.tvshow.RecentlyBrowsedTvShow
 @Dao
 interface RecentlyBrowsedTvShowsDao {
     @Query("SELECT * FROM RecentlyBrowsedTvShow ORDER BY added_date DESC")
-    fun recentBrowsedTvSeries(): DataSource.Factory<Int, RecentlyBrowsedTvShow>
+    fun recentBrowsedTvShow(): DataSource.Factory<Int, RecentlyBrowsedTvShow>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addRecentlyBrowsedTvSeries(vararg recentlyBrowsedMovie: RecentlyBrowsedTvShow)
+    suspend fun addRecentlyBrowsedTvShow(vararg recentlyBrowsedMovie: RecentlyBrowsedTvShow)
 
     @Query("SELECT COUNT(id) FROM RecentlyBrowsedTvShow")
-    suspend fun recentlyBrowsedTvSeriesCount(): Int
+    suspend fun recentlyBrowsedTvShowCount(): Int
 
     @Query("DELETE FROM RecentlyBrowsedTvShow WHERE id IN (SELECT id FROM RecentlyBrowsedTvShow ORDER BY added_date ASC LIMIT :limit)")
     suspend fun deleteLast(limit: Int = 1)
+
+    suspend fun deleteAndAdd(
+        vararg recentlyBrowsedTvSeries: RecentlyBrowsedTvShow,
+        maxItems: Int = 100
+    ) {
+        val addCount = recentlyBrowsedTvSeries.count()
+        val currentCount = recentlyBrowsedTvShowCount()
+
+        val removeCount = (currentCount + addCount - maxItems).coerceAtLeast(0)
+
+        deleteLast(removeCount)
+        addRecentlyBrowsedTvShow(*recentlyBrowsedTvSeries)
+    }
+
+    @Query("DELETE FROM RecentlyBrowsedTvShow")
+    suspend fun clear()
 }
