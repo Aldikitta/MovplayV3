593552111b0b0b57b7bc2d24b69818d4dedfb4fc
Aldik <andi.kitta77@gmail.com>
2022-07-23 13:22:57 +0800

    MoviesDao

--- app/src/main/java/com/example/movplayv3/data/local/db/movie/MoviesDao.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/movie/MoviesDao.kt
@@ -1,9 +1,7 @@
 package com.example.movplayv3.data.local.db.movie
 
 import androidx.paging.PagingSource
-import androidx.room.Dao
-import androidx.room.Query
-import androidx.room.TypeConverters
+import androidx.room.*
 import com.example.movplayv3.data.model.movie.MovieEntity
 import com.example.movplayv3.data.model.movie.MovieEntityType
 import com.example.movplayv3.utils.MovieEntityTypeConverters
@@ -13,4 +11,7 @@ import com.example.movplayv3.utils.MovieEntityTypeConverters
 interface MoviesDao {
     @Query("SELECT * FROM MovieEntity WHERE type=:type AND language=:language")
     fun getAllMovies(type: MovieEntityType, language: String): PagingSource<Int, MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addMovies(movies: List<MovieEntity>)
 }
