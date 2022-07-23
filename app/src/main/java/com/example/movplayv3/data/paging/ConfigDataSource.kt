42c59f7633225ba2922ba5d189d876287d4046cc
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:45:09 +0800

    ConfigDataSource

--- app/src/main/java/com/example/movplayv3/data/paging/ConfigDataSource.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/ConfigDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.movplayv3.data.paging
 
 import android.content.Context
+import com.example.movplayv3.data.model.Config
 import com.example.movplayv3.data.remote.api.movie.TmdbMoviesApiHelper
 import com.example.movplayv3.data.remote.api.others.TmdbOthersApiHelper
 import com.example.movplayv3.data.remote.api.tvshow.TmdbTvShowsApiHelper
@@ -8,6 +9,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -22,4 +24,5 @@ class ConfigDataSource @Inject constructor(
     private val apiTvShowHelper: TmdbTvShowsApiHelper,
     private val apiOtherHelper: TmdbOthersApiHelper
 ) {
+    private val _config: MutableStateFlow<Config?> = MutableStateFlow(null)
 }
