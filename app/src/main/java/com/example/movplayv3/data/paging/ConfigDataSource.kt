d0da31523f786e4518fbf240312e0f7f4716d86d
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:47:24 +0800

    ConfigDataSource

--- app/src/main/java/com/example/movplayv3/data/paging/ConfigDataSource.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/ConfigDataSource.kt
@@ -1,6 +1,9 @@
 package com.example.movplayv3.data.paging
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.speech.RecognizerIntent
 import com.example.movplayv3.data.model.Config
 import com.example.movplayv3.data.remote.api.movie.TmdbMoviesApiHelper
 import com.example.movplayv3.data.remote.api.others.TmdbOthersApiHelper
@@ -9,7 +12,9 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -25,4 +30,13 @@ class ConfigDataSource @Inject constructor(
     private val apiOtherHelper: TmdbOthersApiHelper
 ) {
     private val _config: MutableStateFlow<Config?> = MutableStateFlow(null)
+
+    @SuppressLint("QueryPermissionsNeeded")
+    val speechToTextAvailable: Flow<Boolean> = flow {
+        val packageManager = context.packageManager
+        val activities: List<*> = packageManager.queryIntentActivities(
+            Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH),
+            0
+        )
+    }
 }
