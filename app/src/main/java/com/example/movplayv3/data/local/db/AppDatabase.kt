f0ebf7da8b146ca0153d8b7a2280e814aebcc8df
Aldik <andi.kitta77@gmail.com>
2022-07-23 13:19:55 +0800

    FavoritesTvShowsDao

--- app/src/main/java/com/example/movplayv3/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/AppDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.movplayv3.data.local.db.movie.FavoritesMoviesDao
+import com.example.movplayv3.data.local.db.tvshow.FavoritesTvShowsDao
 import com.example.movplayv3.data.model.SearchQuery
 import com.example.movplayv3.data.model.movie.*
 import com.example.movplayv3.data.model.tvshow.*
@@ -30,5 +31,5 @@ import com.example.movplayv3.utils.DateConverters
 @TypeConverters(DateConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun favoritesMoviesDao(): FavoritesMoviesDao
-//    abstract fun favoritesTvShowsDao():
+    abstract fun favoritesTvShowsDao(): FavoritesTvShowsDao
 }
