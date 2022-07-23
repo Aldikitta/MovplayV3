35483a347657db01435423b7050a55ae16d39b8d
Aldik <andi.kitta77@gmail.com>
2022-07-23 23:34:16 +0800

    DirectorOtherMoviePagingDataSource

--- app/src/main/java/com/example/movplayv3/data/paging/DirectorOtherMoviePagingDataSource.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/DirectorOtherMoviePagingDataSource.kt
@@ -4,10 +4,15 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.movie.Movie
+import com.example.movplayv3.data.remote.api.movie.TmdbMoviesApiHelper
 import com.example.movplayv3.data.remote.api.others.TmdbOthersApiHelper
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.squareup.moshi.JsonDataException
+import retrofit2.HttpException
+import java.io.IOException
 
 class DirectorOtherMoviePagingDataSource(
-    private val apiOtherHelper: TmdbOthersApiHelper,
+    private val apiMovieHelper: TmdbMoviesApiHelper,
     private val language: String = DeviceLanguage.default.languageCode,
     private val region: String = DeviceLanguage.default.region,
     private val directorId: Int
@@ -20,7 +25,32 @@ class DirectorOtherMoviePagingDataSource(
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        return try {
+            val nextPage = params.key ?: 1
 
+            val movieResponse = apiMovieHelper.getOtherMoviesOfDirector(
+                page = nextPage,
+                isoCode = language,
+                region = region,
+                directorId = directorId
+            )
+
+            val currentPage = movieResponse.page
+            val totalPages = movieResponse.totalPages
+
+            LoadResult.Page(
+                data = movieResponse.movies,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (currentPage + 1 > totalPages) null else currentPage + 1
+            )
+        } catch (e: IOException) {
+            LoadResult.Error(e)
+        } catch (e: HttpException) {
+            LoadResult.Error(e)
+        } catch (e: JsonDataException) {
+            FirebaseCrashlytics.getInstance().recordException(e)
+            LoadResult.Error(e)
+        }
     }
 
 }
