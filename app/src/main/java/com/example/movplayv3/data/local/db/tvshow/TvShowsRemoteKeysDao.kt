8deb026e1662daf5ecb684aaecd2f8c2a054adb5
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:24:32 +0800

    TvShowsRemoteKeysDao

--- app/src/main/java/com/example/movplayv3/data/local/db/tvshow/TvShowsRemoteKeysDao.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/tvshow/TvShowsRemoteKeysDao.kt
@@ -1,10 +1,19 @@
 package com.example.movplayv3.data.local.db.tvshow
 
-import androidx.room.Dao
-import androidx.room.TypeConverters
+import androidx.room.*
+import com.example.movplayv3.data.model.tvshow.TvShowEntityType
+import com.example.movplayv3.data.model.tvshow.TvShowsRemoteKeys
 import com.example.movplayv3.utils.TvShowEntityTypeConverters
 
 @TypeConverters(TvShowEntityTypeConverters::class)
 @Dao
-class TvShowsRemoteKeysDao {
+interface TvShowsRemoteKeysDao {
+    @Query("SELECT * FROM TvShowsRemoteKeys WHERE type=:type AND language=:language")
+    suspend fun getRemoteKey(type: TvShowEntityType, language: String): TvShowsRemoteKeys?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertKey(remoteKey: TvShowsRemoteKeys)
+
+    @Query("DELETE FROM TvShowsRemoteKeys WHERE type=:type AND language=:language")
+    suspend fun deleteRemoteKeysOfType(type: TvShowEntityType, language: String)
 }
