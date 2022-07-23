a5ac3ead3df24269f9506736c86a0109b918ac33
Aldik <andi.kitta77@gmail.com>
2022-07-23 21:19:33 +0800

    TvShowsDetailsDao

--- app/src/main/java/com/example/movplayv3/data/local/db/tvshow/TvShowsDetailsDao.kt
+++ app/src/main/java/com/example/movplayv3/data/local/db/tvshow/TvShowsDetailsDao.kt
@@ -1,10 +1,19 @@
 package com.example.movplayv3.data.local.db.tvshow
 
-import androidx.room.Dao
-import androidx.room.TypeConverters
+import androidx.paging.PagingSource
+import androidx.room.*
+import com.example.movplayv3.data.model.tvshow.TvShowDetailEntity
 import com.example.movplayv3.utils.TvShowEntityTypeConverters
 
 @TypeConverters(TvShowEntityTypeConverters::class)
 @Dao
 interface TvShowsDetailsDao {
+    @Query("SELECT * FROM TvShowDetailEntity WHERE language=:language")
+    fun getAllTvSeries(language: String): PagingSource<Int, TvShowDetailEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addTvSeries(movies: List<TvShowDetailEntity>)
+
+    @Query("DELETE FROM TvShowDetailEntity WHERE language=:language")
+    suspend fun deleteAllTvSeries(language: String)
 }
