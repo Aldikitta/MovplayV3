ab8499af1d3891d94a182f9220137afc8ecb7cc2
Aldik <andi.kitta77@gmail.com>
2022-07-23 13:08:58 +0800

    FavoritesTvShowsDao

---
+++ app/src/main/java/com/example/movplayv3/data/local/db/tvshow/FavoritesTvShowsDao.kt
@@ -0,0 +1,27 @@
+package com.example.movplayv3.data.local.db.tvshow
+
+import androidx.paging.DataSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.movplayv3.data.model.tvshow.TvShowFavorite
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoritesTvShowsDao {
+    @Query("SELECT * FROM TvShowFavorite ORDER BY added_date DESC ")
+    fun favouriteTvShows(): DataSource.Factory<Int, TvShowFavorite>
+
+//    @Insert(onConflict = OnConflictStrategy.REPLACE)
+//    suspend fun likeTvSeries(vararg tvSeriesDetails: TvSeriesFavourite)
+//
+//    @Query("DELETE FROM TvSeriesFavourite WHERE id = :tvSeriesId")
+//    suspend fun unlikeTvSeries(tvSeriesId: Int)
+//
+//    @Query("SELECT id FROM TvSeriesFavourite")
+//    fun favouriteTvSeriesIds(): Flow<List<Int>>
+//
+//    @Query("SELECT COUNT(id) FROM TvSeriesFavourite")
+//    fun favouriteTvSeriesCount(): Flow<Int>
+}
