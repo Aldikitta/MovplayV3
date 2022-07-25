86fc74d27858ec0fd62820f5e05b4988862ba863
Aldik <andi.kitta77@gmail.com>
2022-07-25 13:02:20 +0800

    MoviesRemotePagingMediator

--- app/src/main/java/com/example/movplayv3/data/paging/movie/MoviesRemotePagingMediator.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/movie/MoviesRemotePagingMediator.kt
@@ -4,12 +4,14 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
 import com.example.movplayv3.data.local.db.AppDatabase
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.movie.MovieEntity
 import com.example.movplayv3.data.model.movie.MovieEntityType
 import com.example.movplayv3.data.model.movie.MoviesResponse
 import com.example.movplayv3.data.remote.api.movie.TmdbMoviesApiHelper
+import java.util.concurrent.TimeUnit
 
 @OptIn(ExperimentalPagingApi::class)
 class MoviesRemotePagingMediator(
@@ -28,8 +30,24 @@ class MoviesRemotePagingMediator(
             MovieEntityType.Trending -> apiMovieHelper::getTrendingMovies
             MovieEntityType.Upcoming -> apiMovieHelper::getUpcomingMovies
             MovieEntityType.Popular -> apiMovieHelper::getPopularMovies
-
         }
+
+    override suspend fun initialize(): InitializeAction {
+        val remoteKey = appDatabase.withTransaction {
+            remoteKeysDao.getRemoteKey(
+                type = type,
+                language = deviceLanguage.languageCode
+            )
+        } ?: return InitializeAction.LAUNCH_INITIAL_REFRESH
+
+        val cacheTimeout = TimeUnit.HOURS.convert(1, TimeUnit.MILLISECONDS)
+
+        return if ((System.currentTimeMillis() - remoteKey.lastUpdated) >= cacheTimeout) {
+            InitializeAction.SKIP_INITIAL_REFRESH
+        } else {
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+        }
+    }
 
     override suspend fun load(
         loadType: LoadType,
