d506a1910057b0d6fe13d1d16c225e854a01989d
Aldik <andi.kitta77@gmail.com>
2022-07-25 16:19:18 +0800

    AddRecentlyBrowsedTvShowUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/tvshow/AddRecentlyBrowsedTvShowUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/tvshow/AddRecentlyBrowsedTvShowUseCase.kt
@@ -1,4 +1,7 @@
 package com.example.movplayv3.domain.usecase.interfaces.tvshow
 
+import com.example.movplayv3.data.model.movie.MovieDetails
+
 interface AddRecentlyBrowsedTvShowUseCase {
+    operator fun invoke(details: MovieDetails)
 }
