dc3fef40b49a98d0dd978aee0dd9afe9df92ec0f
Aldik <andi.kitta77@gmail.com>
2022-07-25 16:48:02 +0800

    RecentlyBrowsedRepositoryImpl

--- app/src/main/java/com/example/movplayv3/data/repository/browsed/RecentlyBrowsedRepositoryImpl.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/browsed/RecentlyBrowsedRepositoryImpl.kt
@@ -1,7 +1,18 @@
 package com.example.movplayv3.data.repository.browsed
 
+import com.example.movplayv3.data.local.db.movie.RecentlyBrowsedMoviesDao
+import com.example.movplayv3.data.local.db.tvshow.RecentlyBrowsedTvShowsDao
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class RecentlyBrowsedRepositoryImpl @Inject constructor() {
+class RecentlyBrowsedRepositoryImpl @Inject constructor(
+    private val externalScope: CoroutineScope,
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default,
+    private val recentlyBrowsedMoviesDao: RecentlyBrowsedMoviesDao,
+    private val recentlyBrowsedTvSeriesDao: RecentlyBrowsedTvShowsDao
+) {
 }
