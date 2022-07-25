697797907edd88833570f4c8a9684ad6acf124fe
Aldik <andi.kitta77@gmail.com>
2022-07-25 11:48:40 +0800

    MovieDetailsResponsePagingDataSource

--- app/src/main/java/com/example/movplayv3/data/paging/movie/MovieDetailsResponsePagingDataSource.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/movie/MovieDetailsResponsePagingDataSource.kt
@@ -5,6 +5,10 @@ import androidx.paging.PagingState
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.movie.Movie
 import com.example.movplayv3.data.model.movie.MoviesResponse
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.squareup.moshi.JsonDataException
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class MovieDetailsResponsePagingDataSource @Inject constructor(
@@ -21,6 +25,25 @@ class MovieDetailsResponsePagingDataSource @Inject constructor(
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
-        TODO("Not yet implemented")
+        return try {
+            val nextPage = params.key ?: 1
+            val movieResponse = apiHMovieHelperMethod(movieId, nextPage, language, region)
+
+            val currentPage = movieResponse.page
+            val totalPages = movieResponse.totalPages
+
+            LoadResult.Page(
+                data = movieResponse.movies,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (currentPage + 1 > totalPages) null else currentPage + 1
+            )
+        } catch (e: IOException) {
+            LoadResult.Error(e)
+        } catch (e: HttpException) {
+            LoadResult.Error(e)
+        } catch (e: JsonDataException) {
+            FirebaseCrashlytics.getInstance().recordException(e)
+            LoadResult.Error(e)
+        }
     }
 }
