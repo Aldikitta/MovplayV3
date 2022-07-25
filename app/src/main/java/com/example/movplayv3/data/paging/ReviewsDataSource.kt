eed1565bb7a80592123dee4ee5be4733888dcfe9
Aldik <andi.kitta77@gmail.com>
2022-07-25 13:20:25 +0800

    ReviewsDataSource

--- app/src/main/java/com/example/movplayv3/data/paging/ReviewsDataSource.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/ReviewsDataSource.kt
@@ -10,10 +10,13 @@ class ReviewsDataSource(
     private inline val apiHelperMethod: suspend (Int, Int) -> ReviewsResponse
 ): PagingSource<Int, Review>() {
     override fun getRefreshKey(state: PagingState<Int, Review>): Int? {
-        TODO("Not yet implemented")
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Review> {
-        TODO("Not yet implemented")
+
     }
 }
