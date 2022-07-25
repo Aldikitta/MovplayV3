1c7973ac3489a6cdb245328a0a75c427607cbc25
Aldik <andi.kitta77@gmail.com>
2022-07-25 12:06:22 +0800

    MovieDetailsPagingRemoteMediator

--- app/src/main/java/com/example/movplayv3/data/paging/movie/MovieDetailsPagingRemoteMediator.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/movie/MovieDetailsPagingRemoteMediator.kt
@@ -1,15 +1,44 @@
 package com.example.movplayv3.data.paging.movie
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
 import com.example.movplayv3.data.local.db.AppDatabase
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.movie.MovieDetailEntity
 import com.example.movplayv3.data.remote.api.movie.TmdbMoviesApiHelper
+import java.util.concurrent.TimeUnit
 
+@OptIn(ExperimentalPagingApi::class)
 class MovieDetailsPagingRemoteMediator(
     private val deviceLanguage: DeviceLanguage,
     private val apiMovieHelper: TmdbMoviesApiHelper,
     private val appDatabase: AppDatabase
 ) : RemoteMediator<Int, MovieDetailEntity>() {
+    private val movieDetailsDao = appDatabase.moviesDetailsDao()
+    private val movieDetailsRemoteKeysDao = appDatabase.moviesDetailsRemoteKeys()
+
+    override suspend fun initialize(): InitializeAction {
+        val remoteKey = appDatabase.withTransaction {
+            movieDetailsRemoteKeysDao.getRemoteKey(deviceLanguage.languageCode)
+        } ?: return InitializeAction.LAUNCH_INITIAL_REFRESH
+
+        val cacheTimeout = TimeUnit.HOURS.convert(1, TimeUnit.MILLISECONDS)
+
+        return if ((System.currentTimeMillis() - remoteKey.lastUpdates) >= cacheTimeout) {
+            InitializeAction.SKIP_INITIAL_REFRESH
+        } else {
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+        }
+    }
+
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, MovieDetailEntity>
+    ): MediatorResult {
+        TODO("Not yet implemented")
+    }
 
 }
