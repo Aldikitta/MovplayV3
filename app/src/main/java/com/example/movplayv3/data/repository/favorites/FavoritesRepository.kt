1c68e48f34146cb6124f5a329e1c9ca53fdce3c2
Aldik <andi.kitta77@gmail.com>
2022-07-25 20:12:19 +0800

    FavoritesRepository

--- app/src/main/java/com/example/movplayv3/data/repository/favorites/FavoritesRepository.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/favorites/FavoritesRepository.kt
@@ -1,7 +1,11 @@
 package com.example.movplayv3.data.repository.favorites
 
+import androidx.paging.PagingData
 import com.example.movplayv3.data.model.movie.MovieDetails
+import com.example.movplayv3.data.model.movie.MovieFavorite
 import com.example.movplayv3.data.model.tvshow.TvShowDetails
+import com.example.movplayv3.data.model.tvshow.TvShowFavorite
+import kotlinx.coroutines.flow.Flow
 
 interface FavoritesRepository {
     fun likeMovie(movieDetails: MovieDetails)
@@ -11,4 +15,8 @@ interface FavoritesRepository {
     fun unlikeMovie(movieDetails: MovieDetails)
 
     fun unlikeTvShows(tvShowDetails: TvShowDetails)
+
+    fun favoriteMovies(): Flow<PagingData<MovieFavorite>>
+
+    fun favoriteTvShows(): Flow<PagingData<TvShowFavorite>>
 }
