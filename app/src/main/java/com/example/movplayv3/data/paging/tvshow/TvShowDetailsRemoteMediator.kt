c57c563ab62c114f1cdebe23afe7a1a3e30130aa
Aldik <andi.kitta77@gmail.com>
2022-07-25 13:28:33 +0800

    TvShowDetailsRemoteMediator

--- app/src/main/java/com/example/movplayv3/data/paging/tvshow/TvShowDetailsRemoteMediator.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/tvshow/TvShowDetailsRemoteMediator.kt
@@ -1,10 +1,12 @@
 package com.example.movplayv3.data.paging.tvshow
 
 import androidx.paging.*
+import androidx.room.withTransaction
 import com.example.movplayv3.data.local.db.AppDatabase
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.tvshow.TvShowDetailEntity
 import com.example.movplayv3.data.remote.api.tvshow.TmdbTvShowsApiHelper
+import java.util.concurrent.TimeUnit
 
 @OptIn(ExperimentalPagingApi::class)
 class TvShowDetailsRemoteMediator(
@@ -16,8 +18,19 @@ class TvShowDetailsRemoteMediator(
     private val tvShowDetailsRemoteKeysDao = appDatabase.tvShowsDetailsRemoteKeys()
 
     override suspend fun initialize(): InitializeAction {
-        return super.initialize()
+        val remoteKey = appDatabase.withTransaction {
+            tvShowDetailsRemoteKeysDao.getRemoteKey(deviceLanguage.languageCode)
+        } ?: return InitializeAction.LAUNCH_INITIAL_REFRESH
+
+        val cacheTimeout = TimeUnit.HOURS.convert(1, TimeUnit.MILLISECONDS)
+
+        return if ((System.currentTimeMillis() - remoteKey.lastUpdated) >= cacheTimeout) {
+            InitializeAction.SKIP_INITIAL_REFRESH
+        } else {
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+        }
     }
+
     override suspend fun load(
         loadType: LoadType,
         state: PagingState<Int, TvShowDetailEntity>
