c3ee22ccb47faa7a32ba228ea7e3791958f4fe29
Aldik <andi.kitta77@gmail.com>
2022-07-25 16:29:39 +0800

    GetDeviceLanguageUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/GetDeviceLanguageUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/GetDeviceLanguageUseCase.kt
@@ -1,4 +1,8 @@
 package com.example.movplayv3.domain.usecase.interfaces
 
+import com.example.movplayv3.data.model.DeviceLanguage
+import kotlinx.coroutines.flow.Flow
+
 interface GetDeviceLanguageUseCase {
+    operator fun invoke(): Flow<DeviceLanguage>
 }
