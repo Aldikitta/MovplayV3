90717b0b1f75dfe5901170e035e46f9d9d10fda8
Aldik <andi.kitta77@gmail.com>
2022-07-25 13:11:03 +0800

    MoviesRemotePagingMediator

--- app/src/main/java/com/example/movplayv3/data/paging/movie/MoviesRemotePagingMediator.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/movie/MoviesRemotePagingMediator.kt
@@ -9,8 +9,13 @@ import com.example.movplayv3.data.local.db.AppDatabase
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.movie.MovieEntity
 import com.example.movplayv3.data.model.movie.MovieEntityType
+import com.example.movplayv3.data.model.movie.MoviesRemoteKeys
 import com.example.movplayv3.data.model.movie.MoviesResponse
 import com.example.movplayv3.data.remote.api.movie.TmdbMoviesApiHelper
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.squareup.moshi.JsonDataException
+import retrofit2.HttpException
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 @OptIn(ExperimentalPagingApi::class)
@@ -53,6 +58,78 @@ class MoviesRemotePagingMediator(
         loadType: LoadType,
         state: PagingState<Int, MovieEntity>
     ): MediatorResult {
-        TODO("Not yet implemented")
+        return try {
+            val page = when (loadType) {
+                LoadType.REFRESH -> 1
+                LoadType.PREPEND -> {
+                    return MediatorResult.Success(true)
+                }
+                LoadType.APPEND -> {
+                    val remoteKey = appDatabase.withTransaction {
+                        remoteKeysDao.getRemoteKey(
+                            type = type,
+                            language = deviceLanguage.languageCode
+                        )
+                    } ?: return MediatorResult.Success(true)
+
+                    if (remoteKey.nextPage == null) {
+                        return MediatorResult.Success(true)
+                    }
+
+                    remoteKey.nextPage
+                }
+            }
+
+            val result =
+                apiMovieHelperMethod(page, deviceLanguage.languageCode, deviceLanguage.region)
+
+            appDatabase.withTransaction {
+                if (loadType == LoadType.REFRESH) {
+                    movieCacheDao.deleteMoviesOfType(
+                        type = type,
+                        language = deviceLanguage.languageCode
+                    )
+                    remoteKeysDao.deleteRemoteKeysOfType(
+                        type = type,
+                        language = deviceLanguage.languageCode
+                    )
+                }
+
+                val nextPage = if (result.movies.isNotEmpty()) {
+                    page + 1
+                } else null
+
+                val movieEntities = result.movies.map { movie ->
+                    MovieEntity(
+                        id = movie.id,
+                        type = type,
+                        title = movie.title,
+                        originalTitle = movie.originalTitle,
+                        posterPath = movie.posterPath,
+                        language = deviceLanguage.languageCode
+                    )
+                }
+
+                remoteKeysDao.insertKey(
+                    MoviesRemoteKeys(
+                        language = deviceLanguage.languageCode,
+                        type = type,
+                        nextPage = nextPage,
+                        lastUpdated = System.currentTimeMillis()
+                    )
+                )
+                movieCacheDao.addMovies(movieEntities)
+            }
+            MediatorResult.Success(
+                endOfPaginationReached = result.movies.isEmpty()
+            )
+        } catch (e: IOException) {
+            MediatorResult.Error(e)
+        } catch (e: HttpException) {
+            MediatorResult.Error(e)
+        } catch (e: JsonDataException) {
+            FirebaseCrashlytics.getInstance().recordException(e)
+            MediatorResult.Error(e)
+        }
     }
 }
