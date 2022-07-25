7da6c31b4176a7432b868a92781fb1c2a8ce02f7
Aldik <andi.kitta77@gmail.com>
2022-07-25 11:31:27 +0800

    DiscoverTvShowsPagingDataSource

--- app/src/main/java/com/example/movplayv3/data/paging/tvshow/DiscoverTvShowsPagingDataSource.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/tvshow/DiscoverTvShowsPagingDataSource.kt
@@ -1,4 +1,19 @@
 package com.example.movplayv3.data.paging.tvshow
 
-class DiscoverTvShowsPagingDataSource {
+import com.example.movplayv3.data.model.*
+import com.example.movplayv3.data.remote.api.tvshow.TmdbTvShowsApiHelper
+
+class DiscoverTvShowsPagingDataSource(
+    private val apiTvShowsHelper: TmdbTvShowsApiHelper,
+    private val deviceLanguage: DeviceLanguage,
+    private val sortType: SortType = SortType.Popularity,
+    private val sortOrder: SortOrder = SortOrder.Desc,
+    private val genresParam: GenresParam = GenresParam(genres = emptyList()),
+    private val watchProvidersParam: WatchProvidersParam = WatchProvidersParam(watchProviders = emptyList()),
+    private val voteRange: ClosedFloatingPointRange<Float> = 0f..10f,
+    private val onlyWithPosters: Boolean = false,
+    private val onlyWithScore: Boolean = false,
+    private val onlyWithOverview: Boolean = false,
+    private val airDateRange: DateRange
+) {
 }
