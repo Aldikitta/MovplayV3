2f0fb0669cb0987ea941a6b49dad7baa93dade0b
Aldik <andi.kitta77@gmail.com>
2022-07-25 11:50:52 +0800

    MovieResponsePagingDataSource

--- app/src/main/java/com/example/movplayv3/data/paging/movie/MovieResponsePagingDataSource.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/movie/MovieResponsePagingDataSource.kt
@@ -1,11 +1,24 @@
 package com.example.movplayv3.data.paging.movie
 
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.model.movie.Movie
 import com.example.movplayv3.data.model.movie.MoviesResponse
 
 class MovieResponsePagingDataSource(
     private val language: String = DeviceLanguage.default.languageCode,
     private val region: String = DeviceLanguage.default.region,
-    private inline val apiHelperMethod: suspend (Int, String, String) -> MoviesResponse
-) {
+    private inline val apiMovieHelperMethod: suspend (Int, String, String) -> MoviesResponse
+) :PagingSource<Int, Movie>() {
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        TODO("Not yet implemented")
+    }
 }
