7bd72c739581350b8b68e90979afe06bac7d2520
Aldik <andi.kitta77@gmail.com>
2022-07-25 13:00:10 +0800

    MoviesRemotePagingMediator

--- app/src/main/java/com/example/movplayv3/data/paging/movie/MoviesRemotePagingMediator.kt
+++ app/src/main/java/com/example/movplayv3/data/paging/movie/MoviesRemotePagingMediator.kt
@@ -8,6 +8,7 @@ import com.example.movplayv3.data.local.db.AppDatabase
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.movie.MovieEntity
 import com.example.movplayv3.data.model.movie.MovieEntityType
+import com.example.movplayv3.data.model.movie.MoviesResponse
 import com.example.movplayv3.data.remote.api.movie.TmdbMoviesApiHelper
 
 @OptIn(ExperimentalPagingApi::class)
@@ -17,6 +18,19 @@ class MoviesRemotePagingMediator(
     private val apiMovieHelper: TmdbMoviesApiHelper,
     private val appDatabase: AppDatabase
 ) : RemoteMediator<Int, MovieEntity>() {
+    private val movieCacheDao = appDatabase.moviesDao()
+    private val remoteKeysDao = appDatabase.moviesRemoteKeysDao()
+
+    private val apiMovieHelperMethod: suspend (Int, String, String) -> MoviesResponse =
+        when (type) {
+            MovieEntityType.TopRated -> apiMovieHelper::getTopRatedMovies
+            MovieEntityType.Discover -> apiMovieHelper::discoverMovies
+            MovieEntityType.Trending -> apiMovieHelper::getTrendingMovies
+            MovieEntityType.Upcoming -> apiMovieHelper::getUpcomingMovies
+            MovieEntityType.Popular -> apiMovieHelper::getPopularMovies
+
+        }
+
     override suspend fun load(
         loadType: LoadType,
         state: PagingState<Int, MovieEntity>
