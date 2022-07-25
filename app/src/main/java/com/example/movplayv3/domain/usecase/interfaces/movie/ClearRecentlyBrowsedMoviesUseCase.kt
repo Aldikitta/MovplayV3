121f4cab8eab5056e073e6c653f27aa606338c79
Aldik <andi.kitta77@gmail.com>
2022-07-25 16:17:58 +0800

    ClearRecentlyBrowsedMoviesUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/movie/ClearRecentlyBrowsedMoviesUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/movie/ClearRecentlyBrowsedMoviesUseCase.kt
@@ -1,4 +1,7 @@
 package com.example.movplayv3.domain.usecase.interfaces.movie
 
+import com.example.movplayv3.data.model.movie.MovieDetails
+
 interface ClearRecentlyBrowsedMoviesUseCase {
+    operator fun invoke(details: MovieDetails)
 }
