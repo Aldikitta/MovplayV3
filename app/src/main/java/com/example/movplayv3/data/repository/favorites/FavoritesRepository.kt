cac6edf430554c68f44afdeee5892c418ff7f599
Aldik <andi.kitta77@gmail.com>
2022-07-25 20:10:19 +0800

    FavoritesRepository

--- app/src/main/java/com/example/movplayv3/data/repository/favorites/FavoritesRepository.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/favorites/FavoritesRepository.kt
@@ -1,7 +1,10 @@
 package com.example.movplayv3.data.repository.favorites
 
 import com.example.movplayv3.data.model.movie.MovieDetails
+import com.example.movplayv3.data.model.tvshow.TvShowDetails
 
 interface FavoritesRepository {
     fun likeMovie(movieDetails: MovieDetails)
+
+    fun likeTvShow(tvShowDetails: TvShowDetails)
 }
