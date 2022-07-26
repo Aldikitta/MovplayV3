9251e2fdad4c844d4d78cd48e5eec56e668bccaf
Aldik <andi.kitta77@gmail.com>
2022-07-26 20:04:46 +0800

    MovieRepository

--- app/src/main/java/com/example/movplayv3/data/repository/movie/MovieRepository.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/movie/MovieRepository.kt
@@ -1,4 +1,18 @@
 package com.example.movplayv3.data.repository.movie
 
+import com.example.movplayv3.data.model.*
+
 interface MovieRepository {
+    fun discoverMovies(
+        deviceLanguage: DeviceLanguage,
+        sortType: SortType = SortType.Popularity,
+        sortOrder: SortOrder = SortOrder.Desc,
+        genresParam: GenresParam = GenresParam(genres = emptyList()),
+        watchProvidersParam: WatchProvidersParam = WatchProvidersParam(watchProviders = emptyList()),
+        voteRange: ClosedFloatingPointRange<Float> = 0f..10f,
+        onlyWithPosters: Boolean = false,
+        onlyWithScore: Boolean = false,
+        onlyWithOverview: Boolean = false,
+        releaseDateRange: DateRange = DateRange()
+    )
 }
