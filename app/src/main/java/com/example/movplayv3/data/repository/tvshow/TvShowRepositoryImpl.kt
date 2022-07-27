7f9950e4c1b14b9b49378f562d1c7a868163ee69
Aldik <andi.kitta77@gmail.com>
2022-07-27 16:15:12 +0800

    TvShowRepositoryImpl

--- app/src/main/java/com/example/movplayv3/data/repository/tvshow/TvShowRepositoryImpl.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/tvshow/TvShowRepositoryImpl.kt
@@ -1,4 +1,18 @@
 package com.example.movplayv3.data.repository.tvshow
 
-class TvShowRepositoryImpl {
+import androidx.paging.ExperimentalPagingApi
+import com.example.movplayv3.data.local.db.AppDatabase
+import com.example.movplayv3.data.remote.api.tvshow.TmdbTvShowsApiHelper
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+@OptIn(ExperimentalPagingApi::class)
+class TvShowRepositoryImpl @Inject constructor(
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default,
+    private val apiTvShowHelper: TmdbTvShowsApiHelper,
+    private val appDatabase: AppDatabase
+) {
 }
