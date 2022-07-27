32b229c71c4bfc29983ed2afb93d12fd6ff3dc2b
Aldik <andi.kitta77@gmail.com>
2022-07-27 22:09:00 +0800

    GetFavoriteTvShowsCountUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/tvshow/GetFavoriteTvShowsCountUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/tvshow/GetFavoriteTvShowsCountUseCase.kt
@@ -1,4 +1,7 @@
 package com.example.movplayv3.domain.usecase.interfaces.tvshow
 
+import kotlinx.coroutines.flow.Flow
+
 interface GetFavoriteTvShowsCountUseCase {
+    operator fun invoke(): Flow<Int>
 }
