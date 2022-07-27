18f13e9e1f3c350a111c08f9aaf080d723815b9a
Aldik <andi.kitta77@gmail.com>
2022-07-27 15:31:39 +0800

    PersonRepositoryImpl

--- app/src/main/java/com/example/movplayv3/data/repository/person/PersonRepositoryImpl.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/person/PersonRepositoryImpl.kt
@@ -1,6 +1,11 @@
 package com.example.movplayv3.data.repository.person
 
+import com.example.movplayv3.data.model.CombinedCredits
+import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.model.ExternalIds
+import com.example.movplayv3.data.model.PersonDetails
 import com.example.movplayv3.data.remote.api.others.TmdbOthersApiHelper
+import retrofit2.Call
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -8,4 +13,21 @@ import javax.inject.Singleton
 class PersonRepositoryImpl @Inject constructor(
     private val apiOthersApiHelper: TmdbOthersApiHelper
 ) : PersonRepository {
+    override fun getPersonDetails(
+        personId: Int,
+        deviceLanguage: DeviceLanguage
+    ): Call<PersonDetails> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCombinedCredits(
+        personId: Int,
+        deviceLanguage: DeviceLanguage
+    ): Call<CombinedCredits> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getExternalIds(personId: Int, deviceLanguage: DeviceLanguage): Call<ExternalIds> {
+        TODO("Not yet implemented")
+    }
 }
