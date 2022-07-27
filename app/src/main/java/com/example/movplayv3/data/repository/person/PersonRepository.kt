4d0062a30cb804c8b292390ff48885dce5105bbd
Aldik <andi.kitta77@gmail.com>
2022-07-27 15:25:47 +0800

    PersonRepository

--- app/src/main/java/com/example/movplayv3/data/repository/person/PersonRepository.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/person/PersonRepository.kt
@@ -1,4 +1,12 @@
 package com.example.movplayv3.data.repository.person
 
+import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.model.PersonDetails
+import retrofit2.Call
+
 interface PersonRepository {
+    fun getPersonDetails(
+        personId: Int,
+        deviceLanguage: DeviceLanguage = DeviceLanguage.default
+    ) : Call<PersonDetails>
 }
