c2af779f73fc3c39062aec28c7dfe9d05dc4e261
Aldik <andi.kitta77@gmail.com>
2022-07-27 16:50:54 +0800

    GetDiscoverMoviesUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/movie/GetDiscoverMoviesUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/movie/GetDiscoverMoviesUseCase.kt
@@ -1,7 +1,16 @@
 package com.example.movplayv3.domain.usecase.interfaces.movie
 
+import androidx.paging.PagingData
+import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.model.movie.Movie
+import com.example.movplayv3.ui.screens.discover.movies.MovieFilterState
+import com.example.movplayv3.ui.screens.discover.movies.SortInfo
+import kotlinx.coroutines.flow.Flow
+
 interface GetDiscoverMoviesUseCase {
     operator fun invoke(
         sortInfo: SortInfo,
-    )
+        filterState: MovieFilterState,
+        deviceLanguage: DeviceLanguage
+    ): Flow<PagingData<Movie>>
 }
