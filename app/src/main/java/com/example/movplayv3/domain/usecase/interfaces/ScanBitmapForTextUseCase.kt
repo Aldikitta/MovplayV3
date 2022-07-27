def314af9322f956002bd443de62fc39eada9bf1
Aldik <andi.kitta77@gmail.com>
2022-07-27 23:23:04 +0800

    ScanBitmapForTextUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/ScanBitmapForTextUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/ScanBitmapForTextUseCase.kt
@@ -2,6 +2,7 @@ package com.example.movplayv3.domain.usecase.interfaces
 
 import android.graphics.Bitmap
 import com.example.movplayv3.utils.Roi
+import com.example.movplayv3.utils.TextRecognitionHelper
 import kotlinx.coroutines.flow.Flow
 
 interface ScanBitmapForTextUseCase {
