1e94e1c41b87fa6ae4ff91a50ee524419131f5cd
Aldik <andi.kitta77@gmail.com>
2022-07-27 15:39:10 +0800

    SearchRepository

--- app/src/main/java/com/example/movplayv3/data/repository/search/SearchRepository.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/search/SearchRepository.kt
@@ -1,4 +1,20 @@
 package com.example.movplayv3.data.repository.search
 
+import androidx.paging.PagingData
+import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.model.SearchResult
+import kotlinx.coroutines.flow.Flow
+
 interface SearchRepository {
+    fun multiSearch(
+        query: String,
+        includeAdult: Boolean = false,
+        year: Int? = null,
+        releaseYear: Int? = null,
+        deviceLanguage: DeviceLanguage = DeviceLanguage.default
+    ): Flow<PagingData<SearchResult>>
+
+    suspend fun searchQueries(query: String): List<String>
+
+    fun add
 }
