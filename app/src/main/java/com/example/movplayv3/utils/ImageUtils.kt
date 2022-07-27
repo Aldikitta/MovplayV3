e1d5b94c67d809a530691ad6cfac6e893ae0fa4c
Aldik <andi.kitta77@gmail.com>
2022-07-27 23:08:50 +0800

    MediaSearchQueriesUseCase

--- app/src/main/java/com/example/movplayv3/utils/ImageUtils.kt
+++ app/src/main/java/com/example/movplayv3/utils/ImageUtils.kt
@@ -1,8 +1,57 @@
 package com.example.movplayv3.utils
 
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.core.content.ContextCompat
+import java.nio.ByteBuffer
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
 data class Roi(
     val left: Float,
     val top: Float,
     val width: Float,
     val height: Float
 )
+
+
+fun Bitmap.getRoi(roi: Roi): Bitmap {
+    return Bitmap.createBitmap(
+        this,
+        (roi.left * width).toInt(),
+        (roi.top * height).toInt(),
+        (roi.width * width).toInt(),
+        (roi.height * height).toInt()
+    )
+}
+
+fun ImageProxy.toBitmap(rotation: Float? = null): Bitmap {
+    val buffer: ByteBuffer = planes[0].buffer
+    val bytes = ByteArray(buffer.remaining()).also {
+        buffer.get(it)
+    }
+    val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+
+    return rotation?.let { deg -> bitmap.rotate(deg) } ?: bitmap
+}
+
+fun Bitmap.rotate(degrees: Float): Bitmap {
+    return Bitmap.createBitmap(
+        this,
+        0,
+        0,
+        width,
+        height,
+        android.graphics.Matrix().apply { postRotate(degrees) },
+        true
+    )
+}
+
+suspend fun Context.getCameraProvider(): ProcessCameraProvider =
+    suspendCoroutine { continuation ->
+        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
+            cameraProvider.addListener({
+                continuation.run { resume(cameraProvider.get()) }
+            }, ContextCompat.getMainExecutor(this))
+        }
+    }
