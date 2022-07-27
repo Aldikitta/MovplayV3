cc1323b7720103b3468ce8264b227c9bfd3810cf
Aldik <andi.kitta77@gmail.com>
2022-07-27 16:03:42 +0800

    TvShowRepository

--- app/src/main/java/com/example/movplayv3/data/repository/tvshow/TvShowRepository.kt
+++ app/src/main/java/com/example/movplayv3/data/repository/tvshow/TvShowRepository.kt
@@ -1,5 +1,21 @@
 package com.example.movplayv3.data.repository.tvshow
 
+import androidx.paging.PagingData
+import com.example.movplayv3.data.model.*
+import com.example.movplayv3.data.model.tvshow.TvShow
+import kotlinx.coroutines.flow.Flow
+
 interface TvShowRepository {
-    fun discoverTvShows()
+    fun discoverTvShows(
+        deviceLanguage: DeviceLanguage = DeviceLanguage.default,
+        sortType: SortType = SortType.Popularity,
+        sortOrder: SortOrder = SortOrder.Desc,
+        genresParam: GenresParam = GenresParam(genres = emptyList()),
+        watchProvidersParam: WatchProvidersParam = WatchProvidersParam(watchProviders = emptyList()),
+        voteRange: ClosedFloatingPointRange<Float> = 0f..10f,
+        onlyWithPosters: Boolean = false,
+        onlyWithScore: Boolean = false,
+        onlyWithOverview: Boolean = false,
+        airDateRange: DateRange = DateRange()
+    ): Flow<PagingData<TvShow>>
 }
