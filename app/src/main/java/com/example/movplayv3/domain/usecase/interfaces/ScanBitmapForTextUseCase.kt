1b63622f1533630cbbac80e282d5eeda42b6d9ad
Aldik <andi.kitta77@gmail.com>
2022-07-27 23:08:19 +0800

    MediaSearchQueriesUseCase

---
+++ app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/ScanBitmapForTextUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.movplayv3.domain.usecase.interfaces
+
+import android.graphics.Bitmap
+import com.example.movplayv3.utils.Roi
+import kotlinx.coroutines.flow.Flow
+
+interface ScanBitmapForTextUseCase {
+    operator fun invoke(
+        bitmap: Bitmap,
+        rotation: Float,
+        roi: Roi?
+    ): Flow<TextRecognitionHelper.ScanState>
+}
---
+++ app/src/main/java/com/example/movplayv3/utils/ImageUtils.kt
@@ -0,0 +1,8 @@
+package com.example.movplayv3.utils
+
+data class Roi(
+    val left: Float,
+    val top: Float,
+    val width: Float,
+    val height: Float
+)
