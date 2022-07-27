1b5ab87cedc7aafbf8e6ba8c315ea9989fd1af80
Aldik <andi.kitta77@gmail.com>
2022-07-27 22:00:41 +0800

    GetFavoriteMovieCountUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/movie/GetFavoriteMovieCountUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/interfaces/movie/GetFavoriteMovieCountUseCase.kt
@@ -1,4 +1,7 @@
 package com.example.movplayv3.domain.usecase.interfaces.movie
 
+import kotlinx.coroutines.flow.Flow
+
 interface GetFavoriteMovieCountUseCase {
+    operator fun invoke(): Flow<Int>
 }
