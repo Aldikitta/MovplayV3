651a5ac06a82041ead3ad83c811fb0ff4771172f
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:02:48 +0800

    GetTopRatedTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTopRatedTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTopRatedTvShowsUseCaseImpl.kt
@@ -1,15 +1,23 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
+import androidx.paging.map
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Presentable
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTopRatedTvShowsUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
 import javax.inject.Inject
 
 
-class GetTopRatedTvShowsUseCaseImpl @Inject constructor() : GetTopRatedTvShowsUseCase {
+class GetTopRatedTvShowsUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetTopRatedTvShowsUseCase {
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>> {
-        TODO("Not yet implemented")
+        return tvShowRepository.topRatedTvShows(deviceLanguage)
+            .mapLatest { data -> data.map { it } }
     }
 }
