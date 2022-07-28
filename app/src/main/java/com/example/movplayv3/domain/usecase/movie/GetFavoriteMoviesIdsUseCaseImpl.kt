4f1751f0a00b0bbced236b1b71bdd779ce5beb7b
Aldik <andi.kitta77@gmail.com>
2022-07-28 16:15:45 +0800

    GetDiscoverMoviesUseCaseImpl

---
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetFavoriteMoviesIdsUseCaseImpl.kt
@@ -0,0 +1,15 @@
+package com.example.movplayv3.domain.usecase.movie
+
+import com.example.movplayv3.data.repository.favorites.FavoritesRepository
+import com.example.movplayv3.data.repository.movie.MovieRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.GetFavoriteMoviesIdsUseCase
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetFavoriteMoviesIdsUseCaseImpl @Inject constructor(
+    private val favoritesRepository: FavoritesRepository
+) : GetFavoriteMoviesIdsUseCase {
+    override fun invoke(): Flow<List<Int>> {
+        return favoritesRepository.getFavoriteMoviesIds()
+    }
+}
