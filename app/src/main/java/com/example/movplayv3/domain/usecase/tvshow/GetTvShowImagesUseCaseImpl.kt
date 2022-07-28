5495afe2b111b0e5d1a9f281d3589e779e986e4a
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:48:51 +0800

    GetTvShowImagesUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowImagesUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowImagesUseCaseImpl.kt
@@ -1,8 +1,12 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.Image
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowImagesUseCase
+import javax.inject.Inject
 
-interface GetTvShowImagesUseCaseImpl {
-    suspend operator fun invoke(tvShowId: Int): ApiResponse<List<Image>>
-}
+class GetTvShowImagesUseCaseImpl @Inject constructor() : GetTvShowImagesUseCase {
+    override suspend fun invoke(tvShowId: Int): ApiResponse<List<Image>> {
+        TODO("Not yet implemented")
+    }
+}l
