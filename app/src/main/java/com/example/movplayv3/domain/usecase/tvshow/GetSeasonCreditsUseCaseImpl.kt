6b459737d6d9a8c587f65abffb68bd6ee882ce5a
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:42:42 +0800

    GetSeasonCreditsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonCreditsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonCreditsUseCaseImpl.kt
@@ -1,13 +1,18 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.AggregatedCredits
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetSeasonCreditsUseCase
+import javax.inject.Inject
 
-interface GetSeasonCreditsUseCaseImpl {
-    suspend operator fun invoke(
+class GetSeasonCreditsUseCaseImpl @Inject constructor() : GetSeasonCreditsUseCase {
+    override suspend fun invoke(
         tvShowId: Int,
         seasonNumber: Int,
         deviceLanguage: DeviceLanguage
-    ): ApiResponse<AggregatedCredits>
+    ): ApiResponse<AggregatedCredits> {
+        TODO("Not yet implemented")
+    }
+
 }
