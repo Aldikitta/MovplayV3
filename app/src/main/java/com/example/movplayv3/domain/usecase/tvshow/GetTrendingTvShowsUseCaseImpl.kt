2aca2b016467af8f446fe6084eb13584197469fd
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:03:33 +0800

    GetTrendingTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTrendingTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTrendingTvShowsUseCaseImpl.kt
@@ -1,15 +1,21 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
+import androidx.paging.map
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Presentable
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTrendingTvShowsUseCase
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
 import javax.inject.Inject
 
 
-class GetTrendingTvShowsUseCaseImpl @Inject constructor() : GetTrendingTvShowsUseCase {
+class GetTrendingTvShowsUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetTrendingTvShowsUseCase {
     override fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>> {
-        TODO("Not yet implemented")
+        return tvShowRepository.trendingTvShows(deviceLanguage)
+            .mapLatest { data -> data.map { it } }
     }
 }
