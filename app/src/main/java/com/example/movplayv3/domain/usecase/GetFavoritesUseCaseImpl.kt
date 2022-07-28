f1889c8eea2758b3af5473e2df169e4740a5be12
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:58:30 +0800

    GetFavoritesUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetFavoritesUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetFavoritesUseCaseImpl.kt
@@ -3,8 +3,13 @@ package com.example.movplayv3.domain.usecase
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.FavoriteType
 import com.example.movplayv3.data.model.Presentable
+import com.example.movplayv3.domain.usecase.interfaces.GetFavoritesUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetFavoritesUseCaseImpl {
-    operator fun invoke(type: FavoriteType): Flow<PagingData<Presentable>>
+
+class GetFavoritesUseCaseImpl @Inject constructor() : GetFavoritesUseCase {
+    override fun invoke(type: FavoriteType): Flow<PagingData<Presentable>> {
+        TODO("Not yet implemented")
+    }
 }
