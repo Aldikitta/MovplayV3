9ac43e1121fb2cc62f18833acab71047ef43026a
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:40:21 +0800

    GetOnTheAirTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetOnTheAirTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetOnTheAirTvShowsUseCaseImpl.kt
@@ -1,13 +1,19 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DetailPresentable
 import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetOnTheAirTvShowsUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetOnTheAirTvShowsUseCaseImpl {
-    operator fun invoke(
+
+class GetOnTheAirTvShowsUseCaseImpl @Inject constructor() : GetOnTheAirTvShowsUseCase {
+    override fun invoke(
         deviceLanguage: DeviceLanguage,
-        filterId: Boolean = false
-    ): Flow<PagingData<DetailPresentable>>
+        filterId: Boolean
+    ): Flow<PagingData<DetailPresentable>> {
+        TODO("Not yet implemented")
+    }
+
 }
