c12e19a60242d13932432548fdcbfc1b6ffb97e7
Aldik <andi.kitta77@gmail.com>
2022-07-28 17:15:17 +0800

    GetMovieExternalIdsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieExternalIdsUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.movplayv3.domain.usecase.interfaces.movie
-
-import com.example.movplayv3.data.model.ExternalId
-import com.example.movplayv3.data.remote.api.ApiResponse
-
-interface GetMovieExternalIdsUseCase {
-    suspend operator fun invoke(movieId: Int): ApiResponse<List<ExternalId>>
-}
---
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieExternalIdsUseCaseImpl.kt
@@ -0,0 +1,5 @@
+package com.example.movplayv3.domain.usecase.movie
+
+
+class GetMovieExternalIdsUseCaseImpl {
+}
