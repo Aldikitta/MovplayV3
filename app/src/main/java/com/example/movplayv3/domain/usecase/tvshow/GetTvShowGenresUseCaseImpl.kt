eb84b0b6d766eafee204679b36b5b2047136ba7a
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:48:08 +0800

    GetTvShowGenresUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowGenresUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowGenresUseCaseImpl.kt
@@ -1,8 +1,13 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.Genre
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowGenresUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetTvShowGenresUseCaseImpl {
-    operator fun invoke(): Flow<List<Genre>>
+
+class GetTvShowGenresUseCaseImpl @Inject constructor() : GetTvShowGenresUseCase {
+    override fun invoke(): Flow<List<Genre>> {
+        TODO("Not yet implemented")
+    }
 }
