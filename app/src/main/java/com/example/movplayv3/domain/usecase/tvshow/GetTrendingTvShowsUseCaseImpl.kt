a953b5b9f3e91c700ef1916030fcc8895aa36145
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:05:04 +0800

    GetTvShowDetailsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTrendingTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTrendingTvShowsUseCaseImpl.kt
@@ -6,6 +6,7 @@ import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Presentable
 import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTrendingTvShowsUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.mapLatest
 import javax.inject.Inject
@@ -14,6 +15,7 @@ import javax.inject.Inject
 class GetTrendingTvShowsUseCaseImpl @Inject constructor(
     private val tvShowRepository: TvShowRepository
 ) : GetTrendingTvShowsUseCase {
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>> {
         return tvShowRepository.trendingTvShows(deviceLanguage)
             .mapLatest { data -> data.map { it } }
--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowDetailsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowDetailsUseCaseImpl.kt
@@ -3,16 +3,23 @@ package com.example.movplayv3.domain.usecase.tvshow
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.tvshow.TvShowDetails
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowDetailsUseCase
 import javax.inject.Inject
 
 
-class GetTvShowDetailsUseCaseImpl @Inject constructor() : GetTvShowDetailsUseCase {
+class GetTvShowDetailsUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetTvShowDetailsUseCase {
     override suspend fun invoke(
         tvShowId: Int,
         deviceLanguage: DeviceLanguage
     ): ApiResponse<TvShowDetails> {
-        TODO("Not yet implemented")
+        return tvShowRepository.getTvShowDetails(
+            tvShowId = tvShowId,
+            deviceLanguage = deviceLanguage
+        ).awaitApiResponse()
     }
 
 }
