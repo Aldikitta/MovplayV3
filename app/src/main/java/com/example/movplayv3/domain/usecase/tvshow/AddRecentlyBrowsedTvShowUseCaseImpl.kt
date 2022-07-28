43b709ffc95a3825a274423e97224c8ecdb328ed
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:21:47 +0800

    AddRecentlyBrowsedTvShowUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/AddRecentlyBrowsedTvShowUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/AddRecentlyBrowsedTvShowUseCaseImpl.kt
@@ -1,12 +1,14 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.tvshow.TvShowDetails
+import com.example.movplayv3.data.repository.browsed.RecentlyBrowsedRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.AddRecentlyBrowsedTvShowUseCase
 import javax.inject.Inject
 
-class AddRecentlyBrowsedTvShowUseCaseImpl @Inject constructor() : AddRecentlyBrowsedTvShowUseCase {
+class AddRecentlyBrowsedTvShowUseCaseImpl @Inject constructor(
+    private val recentlyBrowsedRepository: RecentlyBrowsedRepository
+) : AddRecentlyBrowsedTvShowUseCase {
     override fun invoke(details: TvShowDetails) {
-        TODO("Not yet implemented")
+        return recentlyBrowsedRepository.addRecentlyBrowsedTvShows(details)
     }
-
 }
