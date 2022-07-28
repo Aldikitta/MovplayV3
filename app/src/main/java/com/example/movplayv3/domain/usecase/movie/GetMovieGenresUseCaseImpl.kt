b643c5ba3eff3da15afdaa8bb2b0cfb10fb5d7fb
Aldik <andi.kitta77@gmail.com>
2022-07-28 17:20:43 +0800

    GetMovieGenresUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieGenresUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieGenresUseCaseImpl.kt
@@ -1,13 +1,16 @@
 package com.example.movplayv3.domain.usecase.movie
 
 import com.example.movplayv3.data.model.Genre
+import com.example.movplayv3.data.repository.config.ConfigRepository
 import com.example.movplayv3.domain.usecase.interfaces.movie.GetMovieGenresUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
-class GetMovieGenresUseCaseImpl @Inject constructor() : GetMovieGenresUseCase {
+class GetMovieGenresUseCaseImpl @Inject constructor(
+    private val configRepository: ConfigRepository
+) : GetMovieGenresUseCase {
     override fun invoke(): Flow<List<Genre>> {
-        TODO("Not yet implemented")
+        return configRepository.getMoviesGenres()
     }
 }
