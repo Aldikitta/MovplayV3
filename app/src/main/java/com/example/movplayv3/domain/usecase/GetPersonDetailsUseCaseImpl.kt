3cd29ed4529fc1c58d5c68b2020a58827bf87f45
Aldik <andi.kitta77@gmail.com>
2022-07-28 19:00:39 +0800

    GetPersonDetailsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetPersonDetailsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetPersonDetailsUseCaseImpl.kt
@@ -3,10 +3,15 @@ package com.example.movplayv3.domain.usecase
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.PersonDetails
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.domain.usecase.interfaces.GetPersonDetailsUseCase
+import javax.inject.Inject
 
-interface GetPersonDetailsUseCaseImpl {
-    suspend operator fun invoke(
+class GetPersonDetailsUseCaseImpl @Inject constructor() : GetPersonDetailsUseCase{
+    override suspend fun invoke(
         personId: Int,
         deviceLanguage: DeviceLanguage
-    ): ApiResponse<PersonDetails>
+    ): ApiResponse<PersonDetails> {
+        TODO("Not yet implemented")
+    }
+
 }
