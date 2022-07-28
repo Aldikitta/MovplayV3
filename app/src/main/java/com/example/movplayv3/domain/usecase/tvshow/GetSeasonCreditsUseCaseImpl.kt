2958e1acc1e7e5679487fbb884a0cf66c26e39da
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:55:55 +0800

    GetSeasonCreditsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonCreditsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonCreditsUseCaseImpl.kt
@@ -3,16 +3,23 @@ package com.example.movplayv3.domain.usecase.tvshow
 import com.example.movplayv3.data.model.AggregatedCredits
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.season.SeasonRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetSeasonCreditsUseCase
 import javax.inject.Inject
 
-class GetSeasonCreditsUseCaseImpl @Inject constructor() : GetSeasonCreditsUseCase {
+class GetSeasonCreditsUseCaseImpl @Inject constructor(
+    private val seasonRepository: SeasonRepository
+) : GetSeasonCreditsUseCase {
     override suspend fun invoke(
         tvShowId: Int,
         seasonNumber: Int,
         deviceLanguage: DeviceLanguage
     ): ApiResponse<AggregatedCredits> {
-        TODO("Not yet implemented")
+        return seasonRepository.seasonCredits(
+            tvShowId = tvShowId,
+            seasonNumber = seasonNumber,
+            isoCode = deviceLanguage.languageCode
+        ).awaitApiResponse()
     }
-
 }
