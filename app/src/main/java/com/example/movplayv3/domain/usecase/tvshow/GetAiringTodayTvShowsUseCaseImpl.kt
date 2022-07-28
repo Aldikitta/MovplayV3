041fcccb41758ded7589e48bd193b94de2c39f08
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:25:12 +0800

    GetAiringTodayTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetAiringTodayTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetAiringTodayTvShowsUseCaseImpl.kt
@@ -1,15 +1,23 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
+import androidx.paging.map
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Presentable
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetAiringTodayTvShowsUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
 import javax.inject.Inject
 
 
-class GetAiringTodayTvShowsUseCaseImpl @Inject constructor() : GetAiringTodayTvShowsUseCase {
+class GetAiringTodayTvShowsUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetAiringTodayTvShowsUseCase {
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>> {
-        TODO("Not yet implemented")
+        return tvShowRepository.airingTodayTvShows(deviceLanguage)
+            .mapLatest { data -> data.map { it } }
     }
 }
