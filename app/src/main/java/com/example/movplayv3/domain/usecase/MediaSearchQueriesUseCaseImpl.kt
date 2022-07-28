6564cbc6ec6810cc13a5a50438573a1b9eed167e
Aldik <andi.kitta77@gmail.com>
2022-07-28 23:24:57 +0800

    ScanBitmapForTextUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/MediaSearchQueriesUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/MediaSearchQueriesUseCaseImpl.kt
@@ -1,10 +1,13 @@
 package com.example.movplayv3.domain.usecase
 
+import com.example.movplayv3.data.repository.search.SearchRepository
 import com.example.movplayv3.domain.usecase.interfaces.MediaSearchQueriesUseCase
 import javax.inject.Inject
 
-class MediaSearchQueriesUseCaseImpl @Inject constructor() : MediaSearchQueriesUseCase{
+class MediaSearchQueriesUseCaseImpl @Inject constructor(
+    private val searchRepository: SearchRepository
+) : MediaSearchQueriesUseCase{
     override suspend fun invoke(query: String): List<String> {
-        TODO("Not yet implemented")
+        return searchRepository.searchQueries(query)
     }
 }
--- app/src/main/java/com/example/movplayv3/domain/usecase/ScanBitmapForTextUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/ScanBitmapForTextUseCaseImpl.kt
@@ -1,19 +1,21 @@
 package com.example.movplayv3.domain.usecase
 
 import android.graphics.Bitmap
+import com.example.movplayv3.data.repository.search.SearchRepository
 import com.example.movplayv3.domain.usecase.interfaces.ScanBitmapForTextUseCase
 import com.example.movplayv3.utils.Roi
 import com.example.movplayv3.utils.TextRecognitionHelper
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class ScanBitmapForTextUseCaseImpl @Inject constructor() : ScanBitmapForTextUseCase {
+class ScanBitmapForTextUseCaseImpl @Inject constructor(
+    private val textRecognitionHelper: TextRecognitionHelper
+) : ScanBitmapForTextUseCase {
     override fun invoke(
         bitmap: Bitmap,
         rotation: Float,
         roi: Roi?
     ): Flow<TextRecognitionHelper.ScanState> {
-        TODO("Not yet implemented")
+        return textRecognitionHelper.scanTextFromBitmap(bitmap, rotation, roi)
     }
-
 }
