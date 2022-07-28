a439876025b2d5a586dda855ca8c8f4f7f6c27fc
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:08:31 +0800

    GetTvShowExternalIdsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowExternalIdsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowExternalIdsUseCaseImpl.kt
@@ -1,13 +1,26 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
+import com.example.movplayv3.data.model.ExternalContentType
 import com.example.movplayv3.data.model.ExternalId
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowExternalIdsUseCase
 import javax.inject.Inject
 
-class GetTvShowExternalIdsUseCaseImpl @Inject constructor() : GetTvShowExternalIdsUseCase {
+class GetTvShowExternalIdsUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetTvShowExternalIdsUseCase {
     override suspend fun invoke(tvShowId: Int): ApiResponse<List<ExternalId>> {
-        TODO("Not yet implemented")
-    }
+        val response = tvShowRepository.getExternalIds(tvShowId).awaitApiResponse()
 
+        return when (response) {
+            is ApiResponse.Success -> {
+                val externalIds = response.data?.toExternalIdList(type = ExternalContentType.Tv)
+                ApiResponse.Success(externalIds)
+            }
+            is ApiResponse.Failure -> ApiResponse.Failure(response.apiError)
+            is ApiResponse.Exception -> ApiResponse.Exception(response.exception)
+        }
+    }
 }
