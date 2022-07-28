2f14bf94215bbe68a959c24188b916f60827de41
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:11:56 +0800

    GetTvShowImagesUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowImagesUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowImagesUseCaseImpl.kt
@@ -2,11 +2,24 @@ package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.Image
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowImagesUseCase
 import javax.inject.Inject
 
-class GetTvShowImagesUseCaseImpl @Inject constructor() : GetTvShowImagesUseCase {
+class GetTvShowImagesUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetTvShowImagesUseCase {
     override suspend fun invoke(tvShowId: Int): ApiResponse<List<Image>> {
-        TODO("Not yet implemented")
+        val response = tvShowRepository.tvShowImages(tvShowId).awaitApiResponse()
+
+        return when (response) {
+            is ApiResponse.Success -> {
+                val images = response.data?.backdrops ?: emptyList()
+                ApiResponse.Success(images)
+            }
+            is ApiResponse.Failure -> ApiResponse.Failure(response.apiError)
+            is ApiResponse.Exception -> ApiResponse.Exception(response.exception)
+        }
     }
 }
