2624fa6e4dde22974e0303decebb3b9dc26f6a48
Aldik <andi.kitta77@gmail.com>
2022-07-28 17:25:51 +0800

    GetMovieReviewsCountUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieReviewsCountUseCase.kt
+++
@@ -1,6 +0,0 @@
-package com.example.movplayv3.domain.usecase.movie
-
-import javax.inject.Inject
-
-class GetMovieReviewsCountUseCase @Inject constructor(){
-}
---
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieReviewsCountUseCaseImpl.kt
@@ -0,0 +1,26 @@
+package com.example.movplayv3.domain.usecase.movie
+
+import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.movie.MovieRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.GetMovieReviewsCountUseCase
+import javax.inject.Inject
+
+class GetMovieReviewsCountUseCaseImpl @Inject constructor(
+    private val movieRepository: MovieRepository
+) : GetMovieReviewsCountUseCase {
+    override suspend fun invoke(movieId: Int): ApiResponse<Int> {
+        val response = movieRepository
+            .movieReview(movieId)
+            .awaitApiResponse()
+
+        return when (response) {
+            is ApiResponse.Success -> {
+                val reviewsCount = response.data?.totalResults ?: 0
+                ApiResponse.Success(reviewsCount)
+            }
+            is ApiResponse.Failure -> ApiResponse.Failure(response.apiError)
+            is ApiResponse.Exception -> ApiResponse.Exception(response.exception)
+        }
+    }
+}
