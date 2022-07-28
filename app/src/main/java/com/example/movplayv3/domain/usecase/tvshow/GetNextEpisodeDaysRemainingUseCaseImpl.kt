9c83d5af93c8cea5ea8ace8510ad91dbb73fb931
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:46:05 +0800

    GetNextEpisodeDaysRemainingUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetNextEpisodeDaysRemainingUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetNextEpisodeDaysRemainingUseCaseImpl.kt
@@ -2,12 +2,15 @@ package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetNextEpisodeDaysRemainingUseCase
 import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 
 class GetNextEpisodeDaysRemainingUseCaseImpl @Inject constructor() :
     GetNextEpisodeDaysRemainingUseCase {
     override fun invoke(nextEpisodeAirDate: Date): Long {
-        TODO("Not yet implemented")
+        val millionSeconds = nextEpisodeAirDate.time - Calendar.getInstance().timeInMillis
+        val daysDiff = TimeUnit.MILLISECONDS.toDays(millionSeconds)
+        return if (daysDiff < 0) 0 else daysDiff
     }
 }
