e46cc3c84aa55b5e8455f1eb84e1110ab8052540
Aldik <andi.kitta77@gmail.com>
2022-07-28 17:43:36 +0800

    GetNowPlayingMoviesUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetNowPlayingMoviesUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetNowPlayingMoviesUseCase.kt
@@ -1,13 +1,22 @@
-package com.example.movplayv3.domain.usecase.interfaces.movie
+package com.example.movplayv3.domain.usecase.movie
 
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DetailPresentable
 import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.repository.movie.MovieRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.GetNowPlayingMoviesUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetNowPlayingMoviesUseCase {
-    operator fun invoke(
+class GetNowPlayingMoviesUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+
+) : GetNowPlayingMoviesUseCase {
+    override fun invoke(
         deviceLanguage: DeviceLanguage,
-        filterId: Boolean = false
-    ): Flow<PagingData<DetailPresentable>>
+        filterId: Boolean
+    ): Flow<PagingData<DetailPresentable>> {
+
+    }
+
 }
