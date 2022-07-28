35f9b0e35ad2f9cb5a7e12f737e3214514cb0ca4
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:39:18 +0800

    GetNextEpisodeDaysRemainingUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetNextEpisodeDaysRemainingUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetNextEpisodeDaysRemainingUseCaseImpl.kt
@@ -1,7 +1,13 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetNextEpisodeDaysRemainingUseCase
 import java.util.*
+import javax.inject.Inject
 
-interface GetNextEpisodeDaysRemainingUseCaseImpl {
-    operator fun invoke(nextEpisodeAirDate: Date): Long
+
+class GetNextEpisodeDaysRemainingUseCaseImpl @Inject constructor() :
+    GetNextEpisodeDaysRemainingUseCase {
+    override fun invoke(nextEpisodeAirDate: Date): Long {
+        TODO("Not yet implemented")
+    }
 }
