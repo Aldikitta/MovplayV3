4b24d48dfe934d4149410add958e1a2cfcf001e5
Aldik <andi.kitta77@gmail.com>
2022-07-28 16:22:30 +0800

    GetFavoritesMoviesUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetFavoritesMoviesUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetFavoritesMoviesUseCaseImpl.kt
@@ -1,10 +1,16 @@
 package com.example.movplayv3.domain.usecase.movie
 
+import androidx.paging.PagingData
+import com.example.movplayv3.data.model.movie.MovieFavorite
 import com.example.movplayv3.data.repository.favorites.FavoritesRepository
 import com.example.movplayv3.domain.usecase.interfaces.movie.GetFavoritesMoviesUseCase
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetFavoritesMoviesUseCaseImpl @Inject constructor(
     private val favoritesRepository: FavoritesRepository
 ) : GetFavoritesMoviesUseCase {
+    override fun invoke(): Flow<PagingData<MovieFavorite>> {
+        return favoritesRepository.favoriteMovies()
+    }
 }
