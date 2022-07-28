9d38f0b3a3272962425c1de58827ec23617aade3
Aldik <andi.kitta77@gmail.com>
2022-07-28 23:14:39 +0800

    GetMediaMultiSearchUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetMediaMultiSearchUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetMediaMultiSearchUseCaseImpl.kt
@@ -3,16 +3,19 @@ package com.example.movplayv3.domain.usecase
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.SearchResult
+import com.example.movplayv3.data.repository.search.SearchRepository
 import com.example.movplayv3.domain.usecase.interfaces.GetMediaMultiSearchUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetMediaMultiSearchUseCaseImpl @Inject constructor() : GetMediaMultiSearchUseCase {
+class GetMediaMultiSearchUseCaseImpl @Inject constructor(
+    private val searchRepository: SearchRepository
+) : GetMediaMultiSearchUseCase {
     override fun invoke(
         query: String,
         deviceLanguage: DeviceLanguage
     ): Flow<PagingData<SearchResult>> {
-        TODO("Not yet implemented")
+        return searchRepository.multiSearch(query = query, deviceLanguage = deviceLanguage)
     }
 
 }
