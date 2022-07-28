6389cf8e6db9a4d826ef8ee23061e51ae7739e88
Aldik <andi.kitta77@gmail.com>
2022-07-28 15:50:37 +0800

    AddRecentlyBrowsedMovieUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/AddRecentlyBrowsedMovieUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/AddRecentlyBrowsedMovieUseCaseImpl.kt
@@ -1,9 +1,14 @@
 package com.example.movplayv3.domain.usecase.movie
 
+import com.example.movplayv3.data.model.movie.MovieDetails
 import com.example.movplayv3.data.repository.browsed.RecentlyBrowsedRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.AddRecentlyBrowsedMovieUseCase
 import javax.inject.Inject
 
 class AddRecentlyBrowsedMovieUseCaseImpl @Inject constructor(
     private val recentlyBrowsedRepository: RecentlyBrowsedRepository
-) {
+) : AddRecentlyBrowsedMovieUseCase {
+    override fun invoke(details: MovieDetails) {
+        TODO("Not yet implemented")
+    }
 }
