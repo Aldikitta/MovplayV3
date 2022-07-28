51d032ff8ed2e09867e1bcb231e1ebc35ebe5357
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:40:03 +0800

    GetFavoritesTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetFavoritesTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetFavoritesTvShowsUseCaseImpl.kt
@@ -1,12 +1,17 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
-import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetFavoriteTvShowIdsUseCase
+import androidx.paging.PagingData
+import com.example.movplayv3.data.model.tvshow.TvShowFavorite
+import com.example.movplayv3.data.repository.favorites.FavoritesRepository
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetFavoritesTvShowsUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
-class GetFavoritesTvShowsUseCaseImpl @Inject constructor() : GetFavoriteTvShowIdsUseCase {
-    override fun invoke(): Flow<List<Int>> {
-        TODO("Not yet implemented")
+class GetFavoritesTvShowsUseCaseImpl @Inject constructor(
+    private val favoritesRepository: FavoritesRepository
+) : GetFavoritesTvShowsUseCase {
+    override fun invoke(): Flow<PagingData<TvShowFavorite>> {
+        return favoritesRepository.favoriteTvShows()
     }
 }
