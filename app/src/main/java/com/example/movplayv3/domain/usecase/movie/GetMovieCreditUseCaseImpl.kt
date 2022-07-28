799af0aad65fccfaf37d07e453609ea20bf6a277
Aldik <andi.kitta77@gmail.com>
2022-07-28 17:10:23 +0800

    GetMovieCreditUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieCreditUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieCreditUseCaseImpl.kt
@@ -1,5 +1,9 @@
 package com.example.movplayv3.domain.usecase.movie
 
+import com.example.movplayv3.data.model.Credits
+import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
 import com.example.movplayv3.data.repository.movie.MovieRepository
 import com.example.movplayv3.domain.usecase.interfaces.movie.GetMovieCreditUseCase
 import javax.inject.Inject
@@ -7,5 +11,14 @@ import javax.inject.Inject
 class GetMovieCreditUseCaseImpl @Inject constructor(
     private val movieRepository: MovieRepository
 ) : GetMovieCreditUseCase {
+    override suspend fun invoke(
+        movieId: Int,
+        deviceLanguage: DeviceLanguage
+    ): ApiResponse<Credits> {
+        return movieRepository.movieCredits(
+            movieId = movieId,
+            isoCode = deviceLanguage.languageCode
+        ).awaitApiResponse()
+    }
 
 }
