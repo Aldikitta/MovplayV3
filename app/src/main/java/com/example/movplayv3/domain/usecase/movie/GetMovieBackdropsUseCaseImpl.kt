4ce0a2839566c9ec52e33bfa7c1068a5768b6e25
Aldik <andi.kitta77@gmail.com>
2022-07-28 16:29:54 +0800

    GetMovieBackdropsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieBackdropsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieBackdropsUseCaseImpl.kt
@@ -5,7 +5,6 @@ import com.example.movplayv3.data.remote.api.ApiResponse
 import com.example.movplayv3.data.remote.api.awaitApiResponse
 import com.example.movplayv3.data.repository.movie.MovieRepository
 import com.example.movplayv3.domain.usecase.interfaces.movie.GetMovieBackdropsUseCase
-import retrofit2.awaitResponse
 import javax.inject.Inject
 
 class GetMovieBackdropsUseCaseImpl @Inject constructor(
@@ -13,5 +12,14 @@ class GetMovieBackdropsUseCaseImpl @Inject constructor(
 ) : GetMovieBackdropsUseCase {
     override suspend fun invoke(movieId: Int): ApiResponse<List<Image>> {
         val response = movieRepository.movieImages(movieId).awaitApiResponse()
+
+        return when (response) {
+            is ApiResponse.Success -> {
+                val backdrops = response.data?.backdrops ?: emptyList()
+                ApiResponse.Success(backdrops)
+            }
+            is ApiResponse.Failure -> ApiResponse.Failure(response.apiError)
+            is ApiResponse.Exception -> ApiResponse.Exception(response.exception)
+        }
     }
 }
