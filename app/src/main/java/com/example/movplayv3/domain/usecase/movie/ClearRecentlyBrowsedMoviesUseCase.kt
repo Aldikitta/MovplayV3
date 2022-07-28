bd761dd0e4bbca3a2a8d02f874406e2d827f7e4b
Aldik <andi.kitta77@gmail.com>
2022-07-28 15:52:17 +0800

    ClearRecentlyBrowsedMoviesUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/ClearRecentlyBrowsedMoviesUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movplayv3.domain.usecase.interfaces.movie
-
-import com.example.movplayv3.data.model.movie.MovieDetails
-
-interface ClearRecentlyBrowsedMoviesUseCase {
-    operator fun invoke()
-}
---
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/ClearRecentlyBrowsedMoviesUseCaseImpl.kt
@@ -0,0 +1,9 @@
+package com.example.movplayv3.domain.usecase.movie
+
+import com.example.movplayv3.data.model.movie.MovieDetails
+import javax.inject.Inject
+
+class ClearRecentlyBrowsedMoviesUseCaseImpl @Inject constructor(
+
+) {
+}
