ec60f42bc0aa11fadca02987655f76d608e737ee
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:42:48 +0800

    GetFavoriteTvShowsCountUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetFavoriteTvShowsCountUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetFavoriteTvShowsCountUseCaseImpl.kt
@@ -1,11 +1,14 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
+import com.example.movplayv3.data.repository.favorites.FavoritesRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetFavoriteTvShowsCountUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetFavoriteTvShowsCountUseCaseImpl @Inject constructor() : GetFavoriteTvShowsCountUseCase {
+class GetFavoriteTvShowsCountUseCaseImpl @Inject constructor(
+    private val favoritesRepository: FavoritesRepository
+) : GetFavoriteTvShowsCountUseCase {
     override fun invoke(): Flow<Int> {
-        TODO("Not yet implemented")
+        return favoritesRepository.getFavoriteTvShowsCount()
     }
 }
