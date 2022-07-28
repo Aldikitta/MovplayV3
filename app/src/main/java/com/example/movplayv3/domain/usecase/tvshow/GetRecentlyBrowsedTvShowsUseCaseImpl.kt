57b93109986f2706b0e27612cd994e5479e5956e
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:41:11 +0800

    GetRecentlyBrowsedTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetRecentlyBrowsedTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetRecentlyBrowsedTvShowsUseCaseImpl.kt
@@ -1,9 +1,14 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.tvshow.RecentlyBrowsedTvShow
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetRecentlyBrowsedTvShowsUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetRecentlyBrowsedTvShowsUseCaseImpl {
-    operator fun invoke(): Flow<PagingData<RecentlyBrowsedTvShow>>
+class GetRecentlyBrowsedTvShowsUseCaseImpl @Inject constructor() :
+    GetRecentlyBrowsedTvShowsUseCase {
+    override fun invoke(): Flow<PagingData<RecentlyBrowsedTvShow>> {
+        TODO("Not yet implemented")
+    }
 }
