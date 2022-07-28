d616d2942f644176a2b5ee380326855234f2113d
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:57:00 +0800

    GetSeasonDetailsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonDetailsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonDetailsUseCaseImpl.kt
@@ -3,16 +3,23 @@ package com.example.movplayv3.domain.usecase.tvshow
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.SeasonDetails
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.season.SeasonRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetSeasonDetailsUseCase
 import javax.inject.Inject
 
-class GetSeasonDetailsUseCaseImpl @Inject constructor() : GetSeasonDetailsUseCase {
+class GetSeasonDetailsUseCaseImpl @Inject constructor(
+    private val seasonRepository: SeasonRepository
+) : GetSeasonDetailsUseCase {
     override suspend fun invoke(
         tvShowId: Int,
         seasonNumber: Int,
         deviceLanguage: DeviceLanguage
     ): ApiResponse<SeasonDetails> {
-        TODO("Not yet implemented")
+        return seasonRepository.seasonDetails(
+            tvShowId = tvShowId,
+            seasonNumber = seasonNumber,
+            deviceLanguage = deviceLanguage
+        ).awaitApiResponse()
     }
-
 }
