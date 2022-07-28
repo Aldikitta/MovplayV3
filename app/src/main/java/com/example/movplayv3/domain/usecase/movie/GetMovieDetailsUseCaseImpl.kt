8ecad5da3f50dcd1571adc4efbddd20a8948b2b4
Aldik <andi.kitta77@gmail.com>
2022-07-28 17:14:05 +0800

    GetMovieDetailsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieDetailsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieDetailsUseCaseImpl.kt
@@ -3,17 +3,21 @@ package com.example.movplayv3.domain.usecase.movie
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.movie.MovieDetails
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.movie.MovieRepository
 import com.example.movplayv3.domain.usecase.interfaces.movie.GetMovieDetailsUseCase
 import javax.inject.Inject
 
 class GetMovieDetailsUseCaseImpl @Inject constructor(
-
+    private val movieRepository: MovieRepository
 ) : GetMovieDetailsUseCase {
     override suspend fun invoke(
         movieId: Int,
         deviceLanguage: DeviceLanguage
     ): ApiResponse<MovieDetails> {
-        TODO("Not yet implemented")
+        return movieRepository.movieDetails(
+            movieId = movieId,
+            isoCode = deviceLanguage.languageCode
+        ).awaitApiResponse()
     }
-
 }
