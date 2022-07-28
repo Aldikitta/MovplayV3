e76bf5d5fa3b8a89f975cc574b417165ddd00045
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:42:00 +0800

    GetRelatedTvShowsOfTypeUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetRelatedTvShowsOfTypeUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetRelatedTvShowsOfTypeUseCaseImpl.kt
@@ -1,15 +1,20 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.RelationType
 import com.example.movplayv3.data.model.tvshow.TvShow
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetRelatedTvShowsOfTypeUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetRelatedTvShowsOfTypeUseCaseImpl {
-    operator fun invoke(
+class GetRelatedTvShowsOfTypeUseCaseImpl @Inject constructor() : GetRelatedTvShowsOfTypeUseCase {
+    override fun invoke(
         tvShowId: Int,
         type: RelationType,
         deviceLanguage: DeviceLanguage
-    ): Flow<PagingData<TvShow>>
+    ): Flow<PagingData<TvShow>> {
+        TODO("Not yet implemented")
+    }
+
 }
