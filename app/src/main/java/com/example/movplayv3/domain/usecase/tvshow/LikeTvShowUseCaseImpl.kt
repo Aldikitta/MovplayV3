a9ba7b7566b8f34af837cc75158e8f2c65462c38
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:29:42 +0800

    UnlikeTvShowUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/LikeTvShowUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/LikeTvShowUseCaseImpl.kt
@@ -1,11 +1,14 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.tvshow.TvShowDetails
+import com.example.movplayv3.data.repository.favorites.FavoritesRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.LikeTvShowUseCase
 import javax.inject.Inject
 
-class LikeTvShowUseCaseImpl @Inject constructor() : LikeTvShowUseCase {
+class LikeTvShowUseCaseImpl @Inject constructor(
+    private val favoritesRepository: FavoritesRepository
+) : LikeTvShowUseCase {
     override fun invoke(details: TvShowDetails) {
-        TODO("Not yet implemented")
+        return favoritesRepository.likeTvShow(details)
     }
 }
--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/UnlikeTvShowUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/UnlikeTvShowUseCaseImpl.kt
@@ -1,11 +1,14 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.tvshow.TvShowDetails
+import com.example.movplayv3.data.repository.favorites.FavoritesRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.UnlikeTvShowUseCase
 import javax.inject.Inject
 
-class UnlikeTvShowUseCaseImpl @Inject constructor() : UnlikeTvShowUseCase {
+class UnlikeTvShowUseCaseImpl @Inject constructor(
+    private val favoritesRepository: FavoritesRepository
+) : UnlikeTvShowUseCase {
     override fun invoke(details: TvShowDetails) {
-        TODO("Not yet implemented")
+        return favoritesRepository.unlikeTvShows(details)
     }
 }
