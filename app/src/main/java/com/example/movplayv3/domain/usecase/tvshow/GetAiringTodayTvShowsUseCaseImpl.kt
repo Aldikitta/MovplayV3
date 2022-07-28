db5660385a522d2a89ed5adbeb2c737212b73e35
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:32:34 +0800

    GetAiringTodayTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetAiringTodayTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetAiringTodayTvShowsUseCaseImpl.kt
@@ -1,10 +1,15 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Presentable
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetAiringTodayTvShowsUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetAiringTodayTvShowsUseCaseImpl {
-    operator fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>>
+
+class GetAiringTodayTvShowsUseCaseImpl @Inject constructor() : GetAiringTodayTvShowsUseCase {
+    override fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>> {
+        TODO("Not yet implemented")
+    }
 }
