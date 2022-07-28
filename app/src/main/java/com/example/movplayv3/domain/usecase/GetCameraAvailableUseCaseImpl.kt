8a9213f58ebba8487637ab1bafacc20b7fcbb888
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:32:04 +0800

    GetCameraAvailableUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetCameraAvailableUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetCameraAvailableUseCaseImpl.kt
@@ -1,11 +1,14 @@
 package com.example.movplayv3.domain.usecase
 
+import com.example.movplayv3.data.repository.config.ConfigRepository
 import com.example.movplayv3.domain.usecase.interfaces.GetCameraAvailableUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCameraAvailableUseCaseImpl @Inject constructor() : GetCameraAvailableUseCase {
+class GetCameraAvailableUseCaseImpl @Inject constructor(
+    private val configRepository: ConfigRepository
+) : GetCameraAvailableUseCase {
     override fun invoke(): Flow<Boolean> {
-        TODO("Not yet implemented")
+        return configRepository.getCameraAvailable()
     }
 }
