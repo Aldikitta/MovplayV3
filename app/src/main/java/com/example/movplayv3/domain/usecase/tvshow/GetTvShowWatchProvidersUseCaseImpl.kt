6aaeab7d1e1e8d5cbf50d852e25cb66af9162340
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:52:33 +0800

    GetTvShowVideosUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowWatchProvidersUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowWatchProvidersUseCaseImpl.kt
@@ -1,12 +1,17 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.WatchProviders
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowWatchProvidersUseCase
+import javax.inject.Inject
 
-interface GetTvShowWatchProvidersUseCaseImpl {
-    suspend operator fun invoke(
+class GetTvShowWatchProvidersUseCaseImpl @Inject constructor() : GetTvShowWatchProvidersUseCase {
+    override suspend fun invoke(
         tvShowId: Int,
         deviceLanguage: DeviceLanguage
-    ): ApiResponse<WatchProviders?>
+    ): ApiResponse<WatchProviders?> {
+        TODO("Not yet implemented")
+    }
+
 }
