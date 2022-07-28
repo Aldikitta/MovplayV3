1cf17b38e7ed201aedd6f4414ce8784ecf676706
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:26:43 +0800

    GetAllTvShowsWatchProvidersUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetAllTvShowsWatchProvidersUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetAllTvShowsWatchProvidersUseCaseImpl.kt
@@ -1,14 +1,17 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.ProviderSource
+import com.example.movplayv3.data.repository.config.ConfigRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetAllTvShowsWatchProvidersUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
-class GetAllTvShowsWatchProvidersUseCaseImpl @Inject constructor() :
+class GetAllTvShowsWatchProvidersUseCaseImpl @Inject constructor(
+    private val configRepository: ConfigRepository
+) :
     GetAllTvShowsWatchProvidersUseCase {
     override fun invoke(): Flow<List<ProviderSource>> {
-        TODO("Not yet implemented")
+        return configRepository.getAllTvShowWatchProviders()
     }
 }
