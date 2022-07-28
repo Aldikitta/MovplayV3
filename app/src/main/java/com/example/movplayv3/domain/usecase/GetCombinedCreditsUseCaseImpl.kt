93554e5013816446e8906e398e0d8bae803f0471
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:56:18 +0800

    GetCombinedCreditsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetCombinedCreditsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetCombinedCreditsUseCaseImpl.kt
@@ -3,10 +3,15 @@ package com.example.movplayv3.domain.usecase
 import com.example.movplayv3.data.model.CombinedCredits
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.domain.usecase.interfaces.GetCombinedCreditsUseCase
+import javax.inject.Inject
 
-interface GetCombinedCreditsUseCaseImpl {
-    suspend operator fun invoke(
+class GetCombinedCreditsUseCaseImpl @Inject constructor() : GetCombinedCreditsUseCase{
+    override suspend fun invoke(
         personId: Int,
         deviceLanguage: DeviceLanguage
-    ): ApiResponse<CombinedCredits>
+    ): ApiResponse<CombinedCredits> {
+        TODO("Not yet implemented")
+    }
+
 }
