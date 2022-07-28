f0740483d09bb5be99cddf274f7ecd427ac687c6
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:04:47 +0800

    GetRelatedMoviesOfTypeUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetRelatedMoviesOfTypeUseCase.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetRelatedMoviesOfTypeUseCase.kt
@@ -1,15 +1,36 @@
-package com.example.movplayv3.domain.usecase.interfaces.movie
+package com.example.movplayv3.domain.usecase.movie
 
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.RelationType
 import com.example.movplayv3.data.model.movie.Movie
+import com.example.movplayv3.data.repository.movie.MovieRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.GetRelatedMoviesOfTypeUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetRelatedMoviesOfTypeUseCase {
-    operator fun invoke(
+class GetRelatedMoviesOfTypeUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) : GetRelatedMoviesOfTypeUseCase {
+    override fun invoke(
         movieId: Int,
         type: RelationType,
         deviceLanguage: DeviceLanguage
-    ): Flow<PagingData<Movie>>
+    ): Flow<PagingData<Movie>> {
+        return when (type) {
+            RelationType.Similar -> {
+                movieRepository.similarMovies(
+                    movieId = movieId,
+                    deviceLanguage = deviceLanguage
+                )
+            }
+            RelationType.Recommended -> {
+                movieRepository.moviesRecommendation(
+                    movieId = movieId,
+                    deviceLanguage = deviceLanguage
+                )
+            }
+        }
+    }
+
 }
