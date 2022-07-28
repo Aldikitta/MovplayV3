c8f34aefff326948ed95f7ad33eb0fa7329d34d9
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:12:27 +0800

    GetTopRatedMoviesUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetTrendingMoviesUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetTrendingMoviesUseCaseImpl.kt
@@ -1,8 +1,24 @@
 package com.example.movplayv3.domain.usecase.movie
 
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.model.Presentable
+import com.example.movplayv3.data.repository.movie.MovieRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.GetTrendingMoviesUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
 import javax.inject.Inject
 
 
-class GetTrendingMoviesUseCaseImpl @Inject constructor() {
+class GetTrendingMoviesUseCaseImpl @Inject constructor(
+    private val movieRepository: MovieRepository
+) : GetTrendingMoviesUseCase {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>> {
+        return movieRepository.trendingMovies(deviceLanguage)
+            .mapLatest { data -> data.map { it } }
+    }
 
 }
