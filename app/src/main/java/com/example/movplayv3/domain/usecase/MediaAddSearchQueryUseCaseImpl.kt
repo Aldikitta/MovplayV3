ae1bd41c20252e31a418c1d8a5f9edabc90b1b4e
Aldik <andi.kitta77@gmail.com>
2022-07-28 23:22:18 +0800

    MediaAddSearchQueryUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/MediaAddSearchQueryUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/MediaAddSearchQueryUseCaseImpl.kt
@@ -1,12 +1,16 @@
 package com.example.movplayv3.domain.usecase
 
 import com.example.movplayv3.data.model.SearchQuery
+import com.example.movplayv3.data.repository.config.ConfigRepository
+import com.example.movplayv3.data.repository.search.SearchRepository
 import com.example.movplayv3.domain.usecase.interfaces.MediaAddSearchQueryUseCase
 import javax.inject.Inject
 
-class MediaAddSearchQueryUseCaseImpl @Inject constructor() : MediaAddSearchQueryUseCase {
+class MediaAddSearchQueryUseCaseImpl @Inject constructor(
+    private val searchRepository: SearchRepository
+) : MediaAddSearchQueryUseCase {
     override fun invoke(searchQuery: SearchQuery) {
-        TODO("Not yet implemented")
+        return searchRepository.addSearchQuery(searchQuery)
     }
 
 }
