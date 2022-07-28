432b0e6d4b173ed38cda16d59d3bb42c6643d56d
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:37:38 +0800

    GetFavoriteTvShowIdsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetFavoriteTvShowIdsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetFavoriteTvShowIdsUseCaseImpl.kt
@@ -1,7 +1,12 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetFavoriteTvShowIdsUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetFavoriteTvShowIdsUseCaseImpl {
-    operator fun invoke(): Flow<List<Int>>
+
+class GetFavoriteTvShowIdsUseCaseImpl @Inject constructor() : GetFavoriteTvShowIdsUseCase {
+    override fun invoke(): Flow<List<Int>> {
+        TODO("Not yet implemented")
+    }
 }
