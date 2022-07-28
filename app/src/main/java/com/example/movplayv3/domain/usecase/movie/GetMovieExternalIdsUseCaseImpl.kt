80e01f73563e0fe5183340eaeaa44eef273e4f8c
Aldik <andi.kitta77@gmail.com>
2022-07-28 17:18:30 +0800

    GetMovieExternalIdsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieExternalIdsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieExternalIdsUseCaseImpl.kt
@@ -1,5 +1,27 @@
 package com.example.movplayv3.domain.usecase.movie
 
+import com.example.movplayv3.data.model.ExternalContentType
+import com.example.movplayv3.data.model.ExternalId
+import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.movie.MovieRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.GetMovieExternalIdsUseCase
+import javax.inject.Inject
 
-class GetMovieExternalIdsUseCaseImpl {
+
+class GetMovieExternalIdsUseCaseImpl @Inject constructor(
+    private val movieRepository: MovieRepository
+) : GetMovieExternalIdsUseCase {
+    override suspend fun invoke(movieId: Int): ApiResponse<List<ExternalId>> {
+        val response = movieRepository.getExternalIds(movieId).awaitApiResponse()
+
+        return when (response) {
+            is ApiResponse.Success -> {
+                val ids = response.data?.toExternalIdList(ExternalContentType.Movie)
+                ApiResponse.Success(ids)
+            }
+            is ApiResponse.Failure -> ApiResponse.Failure(response.apiError)
+            is ApiResponse.Exception -> ApiResponse.Exception(response.exception)
+        }
+    }
 }
