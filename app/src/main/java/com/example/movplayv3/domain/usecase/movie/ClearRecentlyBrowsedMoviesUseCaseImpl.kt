ff0ff4639966d47dc670d6214ef7a092a62352bd
Aldik <andi.kitta77@gmail.com>
2022-07-28 15:55:25 +0800

    ClearRecentlyBrowsedMoviesUseCase

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/ClearRecentlyBrowsedMoviesUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/ClearRecentlyBrowsedMoviesUseCaseImpl.kt
@@ -1,9 +1,13 @@
 package com.example.movplayv3.domain.usecase.movie
 
-import com.example.movplayv3.data.model.movie.MovieDetails
+import com.example.movplayv3.data.repository.browsed.RecentlyBrowsedRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.ClearRecentlyBrowsedMoviesUseCase
 import javax.inject.Inject
 
 class ClearRecentlyBrowsedMoviesUseCaseImpl @Inject constructor(
-
-) {
+    private val recentlyBrowsedRepository: RecentlyBrowsedRepository
+): ClearRecentlyBrowsedMoviesUseCase {
+    override fun invoke() {
+        return recentlyBrowsedRepository.clearRecentlyBrowsedMovies()
+    }
 }
