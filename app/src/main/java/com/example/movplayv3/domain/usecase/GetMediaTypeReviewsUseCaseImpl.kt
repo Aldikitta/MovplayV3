59a208e7139e8718ad7ccfc99be88fa54abb275b
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:59:57 +0800

    GetMediaTypeReviewsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetMediaTypeReviewsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetMediaTypeReviewsUseCaseImpl.kt
@@ -3,11 +3,13 @@ package com.example.movplayv3.domain.usecase
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.MediaType
 import com.example.movplayv3.data.model.Review
+import com.example.movplayv3.domain.usecase.interfaces.GetMediaTypeReviewsUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetMediaTypeReviewsUseCaseImpl {
-    operator fun invoke(
-        mediaId: Int,
-        type: MediaType
-    ): Flow<PagingData<Review>>
+class GetMediaTypeReviewsUseCaseImpl @Inject constructor() : GetMediaTypeReviewsUseCase {
+    override fun invoke(mediaId: Int, type: MediaType): Flow<PagingData<Review>> {
+        TODO("Not yet implemented")
+    }
+
 }
