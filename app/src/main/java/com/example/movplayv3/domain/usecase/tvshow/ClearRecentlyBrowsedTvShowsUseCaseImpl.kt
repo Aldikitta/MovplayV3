174f9a02aaedd7ef64625b1b51796530dd91db89
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:23:03 +0800

    ClearRecentlyBrowsedTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/ClearRecentlyBrowsedTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/ClearRecentlyBrowsedTvShowsUseCaseImpl.kt
@@ -1,10 +1,13 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
+import com.example.movplayv3.data.repository.browsed.RecentlyBrowsedRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.ClearRecentlyBrowsedTvShowsUseCase
 import javax.inject.Inject
 
-class ClearRecentlyBrowsedTvShowsUseCaseImpl @Inject constructor() : ClearRecentlyBrowsedTvShowsUseCase {
+class ClearRecentlyBrowsedTvShowsUseCaseImpl @Inject constructor(
+    private val recentlyBrowsedRepository: RecentlyBrowsedRepository
+) : ClearRecentlyBrowsedTvShowsUseCase {
     override fun invoke() {
-        TODO("Not yet implemented")
+        return recentlyBrowsedRepository.clearRecentlyBrowsedTvShows()
     }
 }
