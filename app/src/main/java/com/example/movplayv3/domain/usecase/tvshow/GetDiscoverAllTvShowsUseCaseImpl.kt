497d3eb74a09115a2bbcd55a1973e33970fddd0d
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:34:36 +0800

    GetDiscoverAllTvShowsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetDiscoverAllTvShowsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetDiscoverAllTvShowsUseCaseImpl.kt
@@ -1,10 +1,15 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Presentable
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetDiscoverAllTvShowsUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetDiscoverAllTvShowsUseCaseImpl {
-    operator fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>>
+
+class GetDiscoverAllTvShowsUseCaseImpl @Inject constructor() : GetDiscoverAllTvShowsUseCase {
+    override fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>> {
+        TODO("Not yet implemented")
+    }
 }
