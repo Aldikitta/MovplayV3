6972b1182cf5dbe49e458d1953a4d2b25c809f9a
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:33:26 +0800

    GetCombinedCreditsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetCombinedCreditsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetCombinedCreditsUseCaseImpl.kt
@@ -3,15 +3,21 @@ package com.example.movplayv3.domain.usecase
 import com.example.movplayv3.data.model.CombinedCredits
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.person.PersonRepository
 import com.example.movplayv3.domain.usecase.interfaces.GetCombinedCreditsUseCase
 import javax.inject.Inject
 
-class GetCombinedCreditsUseCaseImpl @Inject constructor() : GetCombinedCreditsUseCase{
+class GetCombinedCreditsUseCaseImpl @Inject constructor(
+    private val personRepository: PersonRepository
+) : GetCombinedCreditsUseCase {
     override suspend fun invoke(
         personId: Int,
         deviceLanguage: DeviceLanguage
     ): ApiResponse<CombinedCredits> {
-        TODO("Not yet implemented")
+        return personRepository.getCombinedCredits(
+            personId = personId,
+            deviceLanguage = deviceLanguage
+        ).awaitApiResponse()
     }
-
 }
