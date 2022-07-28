6ba4d91dccd45df0597e767d46cc858a4472f204
Aldik <andi.kitta77@gmail.com>
2022-07-28 17:03:27 +0800

    GetMovieCollectionUseCase

---
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetMovieCollectionUseCaseImpl.kt
@@ -0,0 +1,39 @@
+package com.example.movplayv3.domain.usecase.movie
+
+import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.model.movie.MovieCollection
+import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.movie.MovieRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.GetMovieCollectionUseCase
+import javax.inject.Inject
+
+class GetMovieCollectionUseCaseImpl @Inject constructor(
+    private val movieRepository: MovieRepository
+) : GetMovieCollectionUseCase {
+    override suspend fun invoke(
+        collectionId: Int,
+        deviceLanguage: DeviceLanguage
+    ): ApiResponse<MovieCollection?> {
+        val response = movieRepository.collection(
+            collectionId = collectionId,
+            isoCode = deviceLanguage.languageCode
+        ).awaitApiResponse()
+
+        return when (response) {
+            is ApiResponse.Success -> {
+                val collection = response.data?.let { collection ->
+                    val name = collection.name
+                    val parts = collection.parts
+
+                    MovieCollection(
+                        name = name,
+                        parts = parts
+                    )
+                }
+                ApiResponse.Success(collection)
+            }
+        }
+    }
+
+}
