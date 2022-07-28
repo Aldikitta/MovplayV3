a71c3e056321df96d1ad535b510f47c6b211fdb6
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:16:20 +0800

    GetTvShowReviewsCountUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowReviewsCountUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowReviewsCountUseCaseImpl.kt
@@ -1,12 +1,24 @@
 package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowReviewsCountUseCase
 import javax.inject.Inject
 
-class GetTvShowReviewsCountUseCaseImpl @Inject constructor() : GetTvShowReviewsCountUseCase{
+class GetTvShowReviewsCountUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetTvShowReviewsCountUseCase {
     override suspend fun invoke(tvShowId: Int): ApiResponse<Int> {
-        TODO("Not yet implemented")
-    }
+        val response = tvShowRepository.tvShowReview(tvShowId).awaitApiResponse()
 
+        return when (response) {
+            is ApiResponse.Success -> {
+                val reviewCount = response.data?.totalResults ?: 0
+                ApiResponse.Success(reviewCount)
+            }
+            is ApiResponse.Failure -> ApiResponse.Failure(response.apiError)
+            is ApiResponse.Exception -> ApiResponse.Exception(response.exception)
+        }
+    }
 }
