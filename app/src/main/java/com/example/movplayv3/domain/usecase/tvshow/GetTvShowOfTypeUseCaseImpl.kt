b6837827ae2d099ebefccabee28f173ae7e4b687
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:49:33 +0800

    GetTvShowOfTypeUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowOfTypeUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowOfTypeUseCaseImpl.kt
@@ -1,14 +1,20 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Presentable
 import com.example.movplayv3.data.model.tvshow.TvShowType
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowOfTypeUseCase
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-interface GetTvShowOfTypeUseCaseImpl {
-    operator fun invoke(
+
+class GetTvShowOfTypeUseCaseImpl @Inject constructor() : GetTvShowOfTypeUseCase {
+    override fun invoke(
         type: TvShowType,
         deviceLanguage: DeviceLanguage
-    ): Flow<PagingData<Presentable>>
+    ): Flow<PagingData<Presentable>> {
+        TODO("Not yet implemented")
+    }
+
 }
