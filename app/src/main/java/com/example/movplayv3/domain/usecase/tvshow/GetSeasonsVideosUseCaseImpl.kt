88c9a7f1fe010f9aa4d25379a170dfc44f40a974
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:44:26 +0800

    GetSeasonsVideosUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonsVideosUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonsVideosUseCaseImpl.kt
@@ -1,13 +1,19 @@
-package com.example.movplayv3.domain.usecase.interfaces.tvshow
+package com.example.movplayv3.domain.usecase.tvshow
 
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Video
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetSeasonsVideosUseCase
+import javax.inject.Inject
 
-interface GetSeasonsVideosUseCaseImpl {
-    suspend operator fun invoke(
+
+class GetSeasonsVideosUseCaseImpl @Inject constructor() : GetSeasonsVideosUseCase {
+    override suspend fun invoke(
         tvShowId: Int,
         seasonNumber: Int,
         deviceLanguage: DeviceLanguage
-    ): ApiResponse<List<Video>>
+    ): ApiResponse<List<Video>> {
+        TODO("Not yet implemented")
+    }
+
 }
