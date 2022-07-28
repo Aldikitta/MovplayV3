88164b71d7d13393a17bbaf0d880605ae261d59f
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:34:06 +0800

    GetDeviceLanguageUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetDeviceLanguageUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetDeviceLanguageUseCaseImpl.kt
@@ -1,13 +1,16 @@
 package com.example.movplayv3.domain.usecase
 
 import com.example.movplayv3.data.model.DeviceLanguage
+import com.example.movplayv3.data.repository.config.ConfigRepository
 import com.example.movplayv3.domain.usecase.interfaces.GetDeviceLanguageUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
-class GetDeviceLanguageUseCaseImpl @Inject constructor() : GetDeviceLanguageUseCase {
+class GetDeviceLanguageUseCaseImpl @Inject constructor(
+    private val configRepository: ConfigRepository
+) : GetDeviceLanguageUseCase {
     override fun invoke(): Flow<DeviceLanguage> {
-        TODO("Not yet implemented")
+        return configRepository.getDeviceLanguage()
     }
 }
