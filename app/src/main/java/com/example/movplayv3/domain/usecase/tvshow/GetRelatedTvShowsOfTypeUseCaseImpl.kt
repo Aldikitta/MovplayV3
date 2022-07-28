a849fc7f8d81624837d98a883ef2515f0b08ba81
Aldik <andi.kitta77@gmail.com>
2022-07-28 21:53:49 +0800

    GetRelatedTvShowsOfTypeUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetRelatedTvShowsOfTypeUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetRelatedTvShowsOfTypeUseCaseImpl.kt
@@ -4,17 +4,32 @@ import androidx.paging.PagingData
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.RelationType
 import com.example.movplayv3.data.model.tvshow.TvShow
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetRelatedTvShowsOfTypeUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetRelatedTvShowsOfTypeUseCaseImpl @Inject constructor() : GetRelatedTvShowsOfTypeUseCase {
+class GetRelatedTvShowsOfTypeUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetRelatedTvShowsOfTypeUseCase {
     override fun invoke(
         tvShowId: Int,
         type: RelationType,
         deviceLanguage: DeviceLanguage
     ): Flow<PagingData<TvShow>> {
-        TODO("Not yet implemented")
+        return when (type) {
+            RelationType.Similar -> {
+                tvShowRepository.similarTvShows(
+                    tvShowId = tvShowId,
+                    deviceLanguage = deviceLanguage
+                )
+            }
+            RelationType.Recommended -> {
+                tvShowRepository.tvShowsRecommendations(
+                    tvShowId = tvShowId,
+                    deviceLanguage = deviceLanguage
+                )
+            }
+        }
     }
-
 }
