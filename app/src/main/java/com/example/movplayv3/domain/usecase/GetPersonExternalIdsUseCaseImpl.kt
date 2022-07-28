3993f7d39b8b48df802ebc784162848705269eda
Aldik <andi.kitta77@gmail.com>
2022-07-28 23:19:44 +0800

    GetPersonExternalIdsUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetPersonExternalIdsUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetPersonExternalIdsUseCaseImpl.kt
@@ -3,15 +3,20 @@ package com.example.movplayv3.domain.usecase
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.ExternalIds
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.person.PersonRepository
 import com.example.movplayv3.domain.usecase.interfaces.GetPersonExternalIdsUseCase
 import javax.inject.Inject
 
-class GetPersonExternalIdsUseCaseImpl @Inject constructor() : GetPersonExternalIdsUseCase {
+class GetPersonExternalIdsUseCaseImpl @Inject constructor(
+    private val personRepository: PersonRepository
+) : GetPersonExternalIdsUseCase {
     override suspend fun invoke(
         personId: Int,
         deviceLanguage: DeviceLanguage
     ): ApiResponse<ExternalIds> {
-        TODO("Not yet implemented")
+        return personRepository.getExternalIds(personId = personId, deviceLanguage = deviceLanguage)
+            .awaitApiResponse()
     }
 
 }
