7d0d0f69f49e09dc709f64cc22b9bff6bcf2f578
Aldik <andi.kitta77@gmail.com>
2022-07-28 23:20:42 +0800

    GetSpeechToTextAvailableUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/GetSpeechToTextAvailableUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/GetSpeechToTextAvailableUseCaseImpl.kt
@@ -1,11 +1,14 @@
 package com.example.movplayv3.domain.usecase
 
+import com.example.movplayv3.data.repository.config.ConfigRepository
 import com.example.movplayv3.domain.usecase.interfaces.GetSpeechToTextAvailableUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetSpeechToTextAvailableUseCaseImpl @Inject constructor() : GetSpeechToTextAvailableUseCase{
+class GetSpeechToTextAvailableUseCaseImpl @Inject constructor(
+    private val configRepository: ConfigRepository
+) : GetSpeechToTextAvailableUseCase{
     override fun invoke(): Flow<Boolean> {
-        TODO("Not yet implemented")
+        return configRepository.getSpeechToTextAvailable()
     }
 }
