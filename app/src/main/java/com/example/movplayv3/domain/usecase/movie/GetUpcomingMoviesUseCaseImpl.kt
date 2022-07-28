c260c852228b5b7ec8b9bea5f21424f02cb716ed
Aldik <andi.kitta77@gmail.com>
2022-07-28 18:14:14 +0800

    GetUpcomingMoviesUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetUpcomingMoviesUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/movie/GetUpcomingMoviesUseCaseImpl.kt
@@ -1,10 +1,22 @@
-package com.example.movplayv3.domain.usecase.interfaces.movie
+package com.example.movplayv3.domain.usecase.movie
 
 import androidx.paging.PagingData
+import androidx.paging.map
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Presentable
+import com.example.movplayv3.data.repository.movie.MovieRepository
+import com.example.movplayv3.domain.usecase.interfaces.movie.GetUpcomingMoviesUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
+import javax.inject.Inject
 
-interface GetUpcomingMoviesUseCaseImpl {
-    operator fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>>
+class GetUpcomingMoviesUseCaseImpl @Inject constructor(
+    private val movieRepository: MovieRepository
+) : GetUpcomingMoviesUseCase {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun invoke(deviceLanguage: DeviceLanguage): Flow<PagingData<Presentable>> {
+        return movieRepository.upcomingMovies(deviceLanguage)
+            .mapLatest { data -> data.map { it } }
+    }
 }
