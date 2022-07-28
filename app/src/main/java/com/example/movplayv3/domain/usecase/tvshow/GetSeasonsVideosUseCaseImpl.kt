a85ca41eff98d54df06f755a3a27e5d4a0ff5f66
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:01:36 +0800

    GetSeasonsVideosUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonsVideosUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetSeasonsVideosUseCaseImpl.kt
@@ -3,17 +3,38 @@ package com.example.movplayv3.domain.usecase.tvshow
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.Video
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.season.SeasonRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetSeasonsVideosUseCase
 import javax.inject.Inject
 
 
-class GetSeasonsVideosUseCaseImpl @Inject constructor() : GetSeasonsVideosUseCase {
+class GetSeasonsVideosUseCaseImpl @Inject constructor(
+    private val seasonRepository: SeasonRepository
+) : GetSeasonsVideosUseCase {
     override suspend fun invoke(
         tvShowId: Int,
         seasonNumber: Int,
         deviceLanguage: DeviceLanguage
     ): ApiResponse<List<Video>> {
-        TODO("Not yet implemented")
-    }
+        val response = seasonRepository.seasonVideos(
+            tvShowId = tvShowId,
+            seasonNumber = seasonNumber,
+        ).awaitApiResponse()
 
+        return when (response) {
+            is ApiResponse.Success -> {
+                val videos = response.data?.results?.sortedWith(
+                    compareBy<Video> { video ->
+                        video.language == deviceLanguage.languageCode
+                    }.thenByDescending { video ->
+                        video.publishedAt
+                    }
+                )
+                ApiResponse.Success(videos)
+            }
+            is ApiResponse.Failure -> ApiResponse.Failure(response.apiError)
+            is ApiResponse.Exception -> ApiResponse.Exception(response.exception)
+        }
+    }
 }
