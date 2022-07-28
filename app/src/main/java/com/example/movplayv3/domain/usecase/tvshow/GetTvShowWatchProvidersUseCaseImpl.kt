ae9254ed45f09e568c836fb172d8492827e16efd
Aldik <andi.kitta77@gmail.com>
2022-07-28 22:28:18 +0800

    GetTvShowWatchProvidersUseCaseImpl

--- app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowWatchProvidersUseCaseImpl.kt
+++ app/src/main/java/com/example/movplayv3/domain/usecase/tvshow/GetTvShowWatchProvidersUseCaseImpl.kt
@@ -3,15 +3,32 @@ package com.example.movplayv3.domain.usecase.tvshow
 import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.data.model.WatchProviders
 import com.example.movplayv3.data.remote.api.ApiResponse
+import com.example.movplayv3.data.remote.api.awaitApiResponse
+import com.example.movplayv3.data.repository.tvshow.TvShowRepository
 import com.example.movplayv3.domain.usecase.interfaces.tvshow.GetTvShowWatchProvidersUseCase
 import javax.inject.Inject
 
-class GetTvShowWatchProvidersUseCaseImpl @Inject constructor() : GetTvShowWatchProvidersUseCase {
+class GetTvShowWatchProvidersUseCaseImpl @Inject constructor(
+    private val tvShowRepository: TvShowRepository
+) : GetTvShowWatchProvidersUseCase {
     override suspend fun invoke(
         tvShowId: Int,
         deviceLanguage: DeviceLanguage
     ): ApiResponse<WatchProviders?> {
-        TODO("Not yet implemented")
+        val response = tvShowRepository.watchProviders(
+            tvShowId = tvShowId
+        ).awaitApiResponse()
+
+        return when (response) {
+            is ApiResponse.Success -> {
+                val results = response.data?.results
+                val providers = results?.getOrElse(deviceLanguage.region) { null }
+
+                ApiResponse.Success(providers)
+            }
+            is ApiResponse.Failure -> ApiResponse.Failure(response.apiError)
+            is ApiResponse.Exception -> ApiResponse.Exception(response.exception)
+        }
     }
 
 }
