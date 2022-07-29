1e9c22502f2cacfc74d2ca98b7314feeddda3361
Aldik <andi.kitta77@gmail.com>
2022-07-29 14:12:15 +0800

    MovplayV3Application

--- app/src/main/java/com/example/movplayv3/MovplayV3Application.kt
+++ app/src/main/java/com/example/movplayv3/MovplayV3Application.kt
@@ -1,8 +1,31 @@
 package com.example.movplayv3
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import com.example.movplayv3.data.initializer.AppInitializers
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MovplayV3Application : Application() {
+class MovplayV3Application : Application(), ImageLoaderFactory {
+    @Inject
+    lateinit var initializers: AppInitializers
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        initializers.init(this)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return imageLoader
+    }
 }
