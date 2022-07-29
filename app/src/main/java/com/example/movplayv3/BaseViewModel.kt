7f9f72daefbebd07e700eec47b8e50bbd6bcaa8e
Aldik <andi.kitta77@gmail.com>
2022-07-29 14:06:30 +0800

    BaseViewModel

--- app/src/main/java/com/example/movplayv3/BaseViewModel.kt
+++ app/src/main/java/com/example/movplayv3/BaseViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.movplayv3
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.*
 
 open class BaseViewModel : ViewModel() {
+    private val _error: MutableSharedFlow<String?> = MutableSharedFlow(replay = 0)
+    val error: StateFlow<String?> = _error.asSharedFlow().stateIn(
+        viewModelScope, SharingStarted.WhileSubscribed(10), null
+    )
 }
