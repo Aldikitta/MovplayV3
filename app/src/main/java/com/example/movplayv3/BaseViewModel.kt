6eaa2abaaf74d896aee5e8ad983cccb6d6dbcefa
Aldik <andi.kitta77@gmail.com>
2022-07-29 14:08:23 +0800

    BaseViewModel

--- app/src/main/java/com/example/movplayv3/BaseViewModel.kt
+++ app/src/main/java/com/example/movplayv3/BaseViewModel.kt
@@ -2,11 +2,28 @@ package com.example.movplayv3
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.movplayv3.data.remote.api.ApiResponse
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 open class BaseViewModel : ViewModel() {
     private val _error: MutableSharedFlow<String?> = MutableSharedFlow(replay = 0)
     val error: StateFlow<String?> = _error.asSharedFlow().stateIn(
         viewModelScope, SharingStarted.WhileSubscribed(10), null
     )
+
+    protected fun <T> onError(response: ApiResponse.Exception<T>) {
+        FirebaseCrashlytics.getInstance().recordException(response.exception)
+
+        viewModelScope.launch {
+            _error.emit(response.exception.localizedMessage)
+        }
+    }
+
+    protected fun <T> onFailure(response: ApiResponse.Failure<T>) {
+        viewModelScope.launch {
+            _error.emit(response.apiError.statusMessage)
+        }
+    }
 }
