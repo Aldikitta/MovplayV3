5bfa4391b9efd5478dbe3e6fb37237e9dd1f6c57
Aldik <andi.kitta77@gmail.com>
2022-07-29 16:35:56 +0800

    AppModule

--- app/src/main/java/com/example/movplayv3/di/AppModule.kt
+++ app/src/main/java/com/example/movplayv3/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.example.movplayv3.di
 
 import android.content.Context
+import coil.ImageLoader
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import com.example.movplayv3.utils.NetworkStatusTracker
 import dagger.Module
 import dagger.Provides
@@ -11,6 +14,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import okhttp3.OkHttpClient
 import javax.inject.Singleton
 
 @Module
@@ -29,4 +33,25 @@ object AppModule {
     @Singleton
     fun provideNetworkStatusTracker(@ApplicationContext context: Context) =
         NetworkStatusTracker(context)
+
+    @Provides
+    @Singleton
+    fun provideImageLoader(@ApplicationContext context: Context): ImageLoader {
+        return ImageLoader.Builder(context)
+            .memoryCache {
+                MemoryCache.Builder(context)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("img_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .okHttpClient {
+                OkHttpClient.Builder().build()
+            }
+            .build()
+    }
 }
