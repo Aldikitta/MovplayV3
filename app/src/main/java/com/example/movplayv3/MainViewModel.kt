d1eec8bafd51e1520b3a3dd284a158205477ac88
Aldik <andi.kitta77@gmail.com>
2022-07-29 14:16:58 +0800

    MainViewModel

--- app/src/main/java/com/example/movplayv3/MainViewModel.kt
+++ app/src/main/java/com/example/movplayv3/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.movplayv3
 
+import com.example.movplayv3.data.repository.config.ConfigRepository
+import com.example.movplayv3.utils.NetworkStatusTracker
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val networkStatusTracker: Networkstat
-){
+    private val networkStatusTracker: NetworkStatusTracker,
+    private val configRepository: ConfigRepository
+) : BaseViewModel() {
 }
