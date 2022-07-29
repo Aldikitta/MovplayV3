89782b325e3e39b157195a1152d8efcf60be5065
Aldik <andi.kitta77@gmail.com>
2022-07-29 16:39:50 +0800

    DatabaseModule

---
+++ app/src/main/java/com/example/movplayv3/di/DatabaseModule.kt
@@ -0,0 +1,10 @@
+package com.example.movplayv3.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+}
