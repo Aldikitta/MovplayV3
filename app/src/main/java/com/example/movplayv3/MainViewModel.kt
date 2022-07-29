265790e844f1bff0e1b42c2c36d2e67ef5d16cfa
Aldik <andi.kitta77@gmail.com>
2022-07-29 14:20:34 +0800

    MainViewModel

--- app/src/main/java/com/example/movplayv3/MainViewModel.kt
+++ app/src/main/java/com/example/movplayv3/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.movplayv3
 
+import com.example.movplayv3.data.model.SnackBarEvent
 import com.example.movplayv3.data.repository.config.ConfigRepository
 import com.example.movplayv3.utils.NetworkStatusTracker
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.mapLatest
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +13,11 @@ class MainViewModel @Inject constructor(
     private val networkStatusTracker: NetworkStatusTracker,
     private val configRepository: ConfigRepository
 ) : BaseViewModel() {
+    private val connectionStatus = networkStatusTracker.connectionStatus
+
+    val networkSnackbarEvent: StateFlow<SnackBarEvent?> = connectionStatus.mapLatest { status ->
+        when (status){
+
+        }
+    }
 }
