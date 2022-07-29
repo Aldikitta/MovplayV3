a8eff45312f9efbcba5ddc323e985fc3160f5faa
Aldik <andi.kitta77@gmail.com>
2022-07-29 15:41:16 +0800

    MovieScreenUIState

--- app/src/main/java/com/example/movplayv3/ui/screens/movie/MovieScreenUIState.kt
+++ app/src/main/java/com/example/movplayv3/ui/screens/movie/MovieScreenUIState.kt
@@ -1,4 +1,15 @@
 package com.example.movplayv3.ui.screens.movie
 
-class MovieScreenUIState {
+import androidx.compose.runtime.Stable
+import androidx.paging.PagingData
+import com.example.movplayv3.data.model.movie.MovieFavorite
+import com.example.movplayv3.data.model.movie.RecentlyBrowsedMovie
+import kotlinx.coroutines.flow.Flow
+
+@Stable
+data class MovieScreenUIState(
+    val favorites: Flow<PagingData<MovieFavorite>>
+    val recentlyBrowsed: Flow<PagingData<RecentlyBrowsedMovie>>
+) {
 }
+
