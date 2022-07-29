b95256816c6bc89a06b7fcd78f47aab78fa8a93d
Aldik <andi.kitta77@gmail.com>
2022-07-29 15:56:52 +0800

    MovieScreenViewModel

--- app/src/main/java/com/example/movplayv3/ui/screens/movie/MovieScreenViewModel.kt
+++ app/src/main/java/com/example/movplayv3/ui/screens/movie/MovieScreenViewModel.kt
@@ -1,20 +1,37 @@
 package com.example.movplayv3.ui.screens.movie
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.example.movplayv3.data.model.DeviceLanguage
 import com.example.movplayv3.domain.usecase.interfaces.GetDeviceLanguageUseCase
 import com.example.movplayv3.domain.usecase.interfaces.movie.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
 class MovieScreenViewModel @Inject constructor(
     private val getDeviceLanguageUseCase: GetDeviceLanguageUseCase,
     private val getNowPlayingMoviesUseCase: GetNowPlayingMoviesUseCase,
-    private val getDiscoverMoviesUseCase: GetDiscoverMoviesUseCase,
+    private val getDiscoverAllMoviesUseCase: GetDiscoverAllMoviesUseCase,
     private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase,
     private val getTrendingMoviesUseCase: GetTrendingMoviesUseCase,
     private val getTopRatedMoviesUseCase: GetTopRatedMoviesUseCase,
     private val favoritesMoviesUseCase: GetFavoritesMoviesUseCase,
     private val getRecentlyBrowsedMoviesUseCase: GetRecentlyBrowsedMoviesUseCase
 ) : ViewModel() {
+    private val deviceLanguage: Flow<DeviceLanguage> = getDeviceLanguageUseCase()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val moviesState: StateFlow<MoviesState> = deviceLanguage.mapLatest { deviceLanguage ->
+        MoviesState(
+            nowPlaying = getNowPlayingMoviesUseCase(deviceLanguage, true).cachedIn(viewModelScope),
+            discover = getDiscoverAllMoviesUseCase(deviceLanguage).cachedIn(viewModelScope),
+            upcoming = getUpcomingMoviesUseCase(deviceLanguage).cachedIn(viewModelScope),
+            trending = getTrendingMoviesUseCase(deviceLanguage).cachedIn(viewModelScope),
+            topRated = getTopRatedMoviesUseCase(deviceLanguage).cachedIn(viewModelScope)
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(10), MoviesState.default)
 }
