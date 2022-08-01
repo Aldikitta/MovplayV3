bf078e381b76f51a96a63d2e7e517d6ec7e888c4
Aldik <andi.kitta77@gmail.com>
2022-08-01 23:20:48 +0800

    TvShowScreenViewModel

--- app/src/main/java/com/example/movplayv3/ui/screens/tvshow/TvShowScreenViewModel.kt
+++ app/src/main/java/com/example/movplayv3/ui/screens/tvshow/TvShowScreenViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.movplayv3.ui.screens.tvshow
 
-class TvShowScreenViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.movplayv3.domain.usecase.interfaces.GetDeviceLanguageUseCase
+import com.example.movplayv3.domain.usecase.interfaces.tvshow.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TvShowScreenViewModel @Inject constructor(
+    private val getDeviceLanguageUseCaseImpl: GetDeviceLanguageUseCase,
+    private val getOnTheAirTvShowsUseCase: GetOnTheAirTvShowsUseCase,
+    private val getDiscoverAllTvShowsUseCase: GetDiscoverAllTvShowsUseCase,
+    private val getTopRatedTvShowsUseCase: GetTopRatedTvShowsUseCase,
+    private val getTrendingTvShowsUseCase: GetTrendingTvShowsUseCase,
+    private val getAiringTodayTvShowsUseCase: GetAiringTodayTvShowsUseCase,
+    private val getFavoriteTvShowsCountUseCase: GetFavoriteTvShowsCountUseCase,
+    private val getRecentlyBrowsedTvShowsUseCase: GetRecentlyBrowsedTvShowsUseCase
+) : ViewModel() {
 }
