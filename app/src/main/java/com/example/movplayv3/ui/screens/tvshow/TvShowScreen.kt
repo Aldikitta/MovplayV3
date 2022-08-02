a46dd88769c2263d3809140fc2c63857078adac4
Aldik <andi.kitta77@gmail.com>
2022-08-02 21:18:23 +0800

    TvShowScreen

--- app/src/main/java/com/example/movplayv3/ui/screens/tvshow/TvShowScreen.kt
+++ app/src/main/java/com/example/movplayv3/ui/screens/tvshow/TvShowScreen.kt
@@ -1,19 +1,64 @@
 package com.example.movplayv3.ui.screens.tvshow
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import com.example.movplayv3.MainViewModel
+import com.example.movplayv3.data.model.tvshow.TvShowType
+import com.example.movplayv3.ui.screens.destinations.TvShowScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.flow.collectLatest
 
 @Destination
 @Composable
-fun TvShowScreen() {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "TvShow")
+fun TvShowScreen(
+    mainViewModel: MainViewModel,
+    viewModel: TvShowScreenViewModel,
+    navigator: DestinationsNavigator
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    val scrollState = rememberScrollState()
+
+    LaunchedEffect(Unit) {
+        mainViewModel.sameBottomBarRoute.collectLatest { sameRoute ->
+            if (sameRoute == TvShowScreenDestination.route) {
+                scrollState.animateScrollTo(0)
+            }
+        }
     }
+
+//    val onTvSeriesClicked: (Int) -> Unit = { tvSeriesId ->
+//        val destination = TvSeriesDetailsScreenDestination(
+//            tvSeriesId = tvSeriesId,
+//            startRoute = TvScreenDestination.route
+//        )
+//
+//        navigator.navigate(destination)
+//    }
+
+//    val onBrowseTvSeriesClicked: (TvSeriesType) -> Unit = { type ->
+//        navigator.navigate(BrowseTvSeriesScreenDestination(type))
+//    }
+//
+//    val onDiscoverTvSeriesClicked = {
+//        navigator.navigate(DiscoverTvSeriesScreenDestination)
+//    }
+}
+
+@Composable
+fun TvShowsScreenContent(
+    uiState: TvShowScreenUIState,
+    scrollState: ScrollState,
+//    onTvSeriesClicked: (tvSeriesId: Int) -> Unit,
+//    onBrowseTvSeriesClicked: (type: TvSeriesType) -> Unit,
+    onDiscoverTvSeriesClicked: () -> Unit
+) {
 }
