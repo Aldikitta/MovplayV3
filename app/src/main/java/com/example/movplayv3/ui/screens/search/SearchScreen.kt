ad681cbe2c60745de1387fbc28a83a7bc7e6a173
Aldik <andi.kitta77@gmail.com>
2022-08-03 12:18:04 +0800

    SearchScreen

--- app/src/main/java/com/example/movplayv3/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/movplayv3/ui/screens/search/SearchScreen.kt
@@ -1,19 +1,51 @@
 package com.example.movplayv3.ui.screens.search
 
+import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.movplayv3.data.model.SearchQuery
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Destination
 @Composable
-fun SearchScreen() {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Search")
-    }
+fun AnimatedVisibilityScope.SearchScreen(
+    viewModel: SearchScreenViewModel = hiltViewModel(),
+    navigator: DestinationsNavigator,
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    val onQueryChanged: (query: String) -> Unit = viewModel::onQueryChange
+    val onQueryCleared: () -> Unit = viewModel::onQueryClear
+    val onAddSearchQuerySuggestions: (SearchQuery) -> Unit = viewModel::addQuerySuggestion
+
+//    val onCameraClicked = {
+//        navigator.navigate(ScannerScreenDestination)
+//    }
+    val onQuerySuggestionSelected: (String) -> Unit = viewModel::onQuerySuggestionSelected
+    SearchScreenContent(
+        uiState = uiState,
+        onQueryChanged = onQueryChanged,
+        onQueryCleared = onQueryCleared,
+        onQuerySuggestionSelected = onQuerySuggestionSelected
+    )
+}
+
+@Composable
+fun SearchScreenContent(
+    uiState: SearchScreenUIState,
+    onQueryChanged: (query: String) -> Unit,
+    onQueryCleared: () -> Unit,
+//    onResultClicked: (id: Int, type: MediaType) -> Unit,
+//    onCameraClicked: () -> Unit = {},
+//    onMovieClicked: (Int) -> Unit,
+    onQuerySuggestionSelected: (String) -> Unit
+) {
+    val focusManager = LocalFocusManager.current
 }
