e6b26c1f99a5ab5cf46093e907bb58dbe9aae181
Aldik <andi.kitta77@gmail.com>
2022-08-04 13:43:26 +0800

    MovieDetailsScreenTransitions

---
+++ app/src/main/java/com/example/movplayv3/ui/screens/details/movie/MovieDetailsScreenTransitions.kt
@@ -0,0 +1,63 @@
+package com.example.movplayv3.ui.screens.details.movie
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.navigation.NavBackStackEntry
+import com.example.movplayv3.ui.screens.destinations.FavoriteScreenDestination
+import com.example.movplayv3.ui.screens.destinations.MovieScreenDestination
+import com.example.movplayv3.ui.screens.destinations.SearchScreenDestination
+import com.ramcosta.composedestinations.spec.DestinationStyle
+
+@OptIn(ExperimentalAnimationApi::class)
+object MovieDetailsScreenTransitions : DestinationStyle.Animated {
+    override fun AnimatedContentScope<NavBackStackEntry>.enterTransition(): EnterTransition? {
+        return when (initialState.destination.route) {
+            MovieScreenDestination.route,
+            FavoriteScreenDestination.route,
+            SearchScreenDestination.route -> slideIntoContainer(
+                towards = AnimatedContentScope.SlideDirection.Up,
+                animationSpec = tween(300)
+            )
+            else -> null
+        }
+    }
+
+    override fun AnimatedContentScope<NavBackStackEntry>.popEnterTransition(): EnterTransition? {
+        return when (initialState.destination.route) {
+            MovieScreenDestination.route,
+            FavoriteScreenDestination.route,
+            SearchScreenDestination.route -> slideIntoContainer(
+                towards = AnimatedContentScope.SlideDirection.Up,
+                animationSpec = tween(300)
+            )
+            else -> null
+        }
+    }
+
+    override fun AnimatedContentScope<NavBackStackEntry>.exitTransition(): ExitTransition? {
+        return when (targetState.destination.route) {
+            MovieScreenDestination.route,
+            FavoriteScreenDestination.route,
+            SearchScreenDestination.route -> slideOutOfContainer(
+                towards = AnimatedContentScope.SlideDirection.Down,
+                animationSpec = tween(300)
+            )
+            else -> null
+        }
+    }
+
+    override fun AnimatedContentScope<NavBackStackEntry>.popExitTransition(): ExitTransition? {
+        return when (targetState.destination.route) {
+            MovieScreenDestination.route,
+            FavoriteScreenDestination.route,
+            SearchScreenDestination.route -> slideOutOfContainer(
+                towards = AnimatedContentScope.SlideDirection.Down,
+                animationSpec = tween(300)
+            )
+            else -> null
+        }
+    }
+}
