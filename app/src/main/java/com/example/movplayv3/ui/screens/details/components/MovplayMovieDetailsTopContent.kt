65f79b370bd99e0e9cc7712a8f5fd7d4ae6985b7
Aldik <andi.kitta77@gmail.com>
2022-08-04 15:42:35 +0800

    MovplayMovieDetailsInfoSection

--- app/src/main/java/com/example/movplayv3/ui/screens/details/components/MovplayMovieDetailsTopContent.kt
+++ app/src/main/java/com/example/movplayv3/ui/screens/details/components/MovplayMovieDetailsTopContent.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.movplayv3.R
 import com.example.movplayv3.data.model.movie.MovieDetails
+import com.example.movplayv3.ui.components.texts.MovplayLabeledText
 import com.example.movplayv3.ui.theme.spacing
+import com.example.movplayv3.utils.formattedMoney
 
 @Composable
 fun MovplayMovieDetailsTopContent(
@@ -18,13 +22,29 @@ fun MovplayMovieDetailsTopContent(
     Crossfade(
         modifier = modifier,
         targetState = movieDetails
-    ) {details ->
-        if (details != null){
+    ) { details ->
+        if (details != null) {
             Column(
                 modifier = Modifier.fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small)
             ) {
+                MovplayLabeledText(
+                    label = stringResource(R.string.movie_details_status),
+                    text = stringResource(details.status.getLabel())
+                )
 
+                if (details.budget > 0) {
+                    MovplayLabeledText(
+                        label = stringResource(R.string.movie_details_budget),
+                        text = details.budget.formattedMoney()
+                    )
+                }
+                if (details.revenue > 0) {
+                    MovplayLabeledText(
+                        label = stringResource(R.string.movie_details_boxoffice),
+                        text = details.revenue.formattedMoney()
+                    )
+                }
             }
         }
     }
