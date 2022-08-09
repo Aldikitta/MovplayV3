e23a23e777a9a9ba76906df37d0dbc8b40df2b6b
Aldik <andi.kitta77@gmail.com>
2022-08-09 12:33:48 +0800

    PersonDetailsViewModel

--- app/src/main/java/com/example/movplayv3/ui/screens/details/components/MovplayPersonDetailsInfoSection.kt
+++ app/src/main/java/com/example/movplayv3/ui/screens/details/components/MovplayPersonDetailsInfoSection.kt
@@ -1,4 +1,54 @@
 package com.example.movplayv3.ui.screens.details.components
 
-class MovplayPersonDetailsInfoSection {
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.movplayv3.data.model.PersonDetails
+import com.example.movplayv3.ui.components.texts.MovplayExpandableText
+import com.example.movplayv3.ui.theme.spacing
+
+@Composable
+fun MovplayPersonDetailsInfoSection(
+    personDetails: PersonDetails?,
+    modifier: Modifier = Modifier
+) {
+    Crossfade(
+        modifier = modifier,
+        targetState = personDetails
+    ) { details ->
+        if (details != null) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = MaterialTheme.spacing.medium),
+                    text = details.name,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+
+                if (details.biography.isNotEmpty()) {
+                    MovplayExpandableText(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = MaterialTheme.spacing.medium),
+                        text = details.biography
+                    )
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/movplayv3/ui/screens/details/components/MovplayPersonDetailsTopContent.kt
+++ app/src/main/java/com/example/movplayv3/ui/screens/details/components/MovplayPersonDetailsTopContent.kt
@@ -1,4 +1,59 @@
 package com.example.movplayv3.ui.screens.details.components
 
-class MovplayPersonDetailsTopContent {
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.movplayv3.R
+import com.example.movplayv3.data.model.PersonDetails
+import com.example.movplayv3.ui.components.texts.MovplayLabeledText
+import com.example.movplayv3.ui.theme.spacing
+import com.example.movplayv3.utils.formatted
+
+@Composable
+fun MovplayPersonDetailsTopContent(
+    personDetails: PersonDetails?,
+    modifier: Modifier = Modifier
+) {
+    Crossfade(
+        modifier = modifier,
+        targetState = personDetails
+    ) { details ->
+        if (details != null) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.small)
+            ) {
+                MovplayLabeledText(
+                    label = stringResource(R.string.person_details_screen_know_for_label),
+                    text = details.knownFor
+                )
+
+                details.birthPlace?.let { birthplace ->
+                    MovplayLabeledText(
+                        label = stringResource(R.string.person_details_screen_birthplace),
+                        text = birthplace
+                    )
+                }
+
+                details.birthday?.let { date ->
+                    MovplayLabeledText(
+                        label = stringResource(R.string.person_details_screen_birthday),
+                        text = date.formatted()
+                    )
+                }
+
+                details.deathDate?.let { date ->
+                    MovplayLabeledText(
+                        label = stringResource(R.string.person_details_screen_death_date),
+                        text = date.formatted()
+                    )
+                }
+            }
+        }
+    }
 }
